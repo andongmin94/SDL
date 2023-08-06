2dffb50a7602d43439d493b7594ba02966ecd02b
안동민 <k1016h@naver.com>
2023-08-06 19:45:04 +0900

    d

---
+++ CODE/Cpp/hong/9.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Your name ?" << endl;
+    string name;
+    // cin >> name;
+    std::getline(std::cin, name);
+
+    cout << "Your age ?" << endl;
+    string age;
+    // cin >> age;
+    std::getline(std::cin, age);
+
+    cout << name << " " << age << endl;
+
+    return 0;
+}
