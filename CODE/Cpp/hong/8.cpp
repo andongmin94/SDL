a6b834c688b08e0566da47d5baeff7f4a1811467
안동민 <k1016h@naver.com>
2023-08-06 19:01:14 +0900

    push

---
+++ CODE/Cpp/hong/8.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    const char my_strs[] = "Hello, World";
+    const string my_hell = "Hello, World!";
+
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
