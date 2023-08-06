dff77377c842035d6f40e543ce81eba98cf39910
안동민 <k1016h@naver.com>
2023-08-06 20:08:04 +0900

    push

---
+++ Cpp/hong/10.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+enum Color // user-defined data types
+{
+    COLOR_BLACK,
+    COLOR_RED,
+    COLOR_BLUE,
+    COLOR_GREEN,
+};
+
+int computeDamage(int weapon_id)
+{
+    if (weapon_id == 0) // sword
+    {
+        return 1;
+    }
+
+    if (weapon_id == 1) // hammer
+    {
+        return 2;
+    }
+}
+
+
+int main()
+{
+
+    return 0;
+}
