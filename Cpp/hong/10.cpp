658ec27427ea5f7332af7f6d76e6b6f616864f3f
안동민 <k1016h@naver.com>
2023-08-07 01:38:01 +0900

    push
    2023-08-07
    오전 01:37
    study

--- Cpp/hong/10.cpp
+++ Cpp/hong/10.cpp
@@ -5,28 +5,31 @@ using namespace std;
 
 enum Color // user-defined data types
 {
-    COLOR_BLACK,
+    COLOR_BLACK = -3,
     COLOR_RED,
     COLOR_BLUE,
     COLOR_GREEN,
+    COLOR_SKYBLUE,
 };
 
 int computeDamage(int weapon_id)
 {
     if (weapon_id == 0) // sword
-    {
+        {
         return 1;
-    }
-
-    if (weapon_id == 1) // hammer
-    {
-        return 2;
-    }
+        }
 }
-
 
 int main()
 {
+    Color paint = COLOR_BLACK;
+    Color house(COLOR_BLUE);
+    Color apple{COLOR_RED};
+
+    if (paint == -3)
+    {
+        cout << 1 << endl;
+    }
 
     return 0;
 }
