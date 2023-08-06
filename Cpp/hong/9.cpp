69d93162a40a411edb97b4ba17cd7139acc7c673
안동민 <k1016h@naver.com>
2023-08-06 20:08:04 +0900

    push

--- Cpp/hong/9.cpp
+++ Cpp/hong/9.cpp
@@ -1,20 +1,14 @@
-﻿#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-    cout << "Your name ?" << endl;
-    string name;
-    // cin >> name;
-    std::getline(std::cin, name);
-
-    cout << "Your age ?" << endl;
-    string age;
-    // cin >> age;
-    std::getline(std::cin, age);
-
-    cout << name << " " << age << endl;
-    return 0;
-}
+﻿// #include <iostream>
+// #include <string>
+//
+// using namespace std;
+//
+// int main()
+// {
+//     string a = "for length check";
+//
+//     cout << a.length() << endl;
+//     cout << a.size() << endl;
+//
+//     return 0;
+// }
