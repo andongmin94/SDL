6b597296036d7e3f9c3e67293e4f8048ce610ab6
안동민 <k1016h@naver.com>
2023-08-07 19:57:27 +0900

    update

---
+++ Cpp/hong/8.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    const char my_strs[] = "Hello, World";
+    const string my_hell("Hello, World!");
+    string my_ID = "123";
+    cout << my_hell << endl;
+
+    return 0;
+}
