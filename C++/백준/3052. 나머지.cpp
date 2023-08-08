959b949d46a551614d247139ba797bdfdb0201d6
안동민 <k1016h@naver.com>
2023-08-09 00:25:54 +0900

    update
    2023-08-09
    오전 12:25

---
+++ C++/백준/3052. 나머지.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<int> s;
+    for (int i = 0; i < 10; i++)
+    {
+        int n;
+        cin >> n;
+        s.insert(n % 42);
+    }
+    cout << s.size();
+}
