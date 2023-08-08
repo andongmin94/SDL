898014b90bae29f2d0e82312d0e425d6164a3cf5
안동민 <k1016h@naver.com>
2023-08-08 15:57:13 +0900

    update
    2023-08-08
    오후 03:57

--- C++/SWEA/1989. 초심자의 회문 검사.cpp
+++ C++/SWEA/1989. 초심자의 회문 검사.cpp
@@ -1,6 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
 
-int main(int argc, char* argv[])
+void reverse()
 {
-    
-    return 0;
+	
+}
+
+int main()
+{
+	using namespace std;
+
+	int T;
+	cin >> T;
+	string* N = new string[T];
+	string* N_reverse = new string[T];
+	for (int i = 0; i < T; i++)
+	{
+		cin >> N[i];
+		N_reverse[i] = N[i];
+	}
+	for (int i = 0; i < T; i++)
+	{
+		reverse(N_reverse[i].begin(), N_reverse[i].end());
+		if (N[i] == N_reverse[i])
+			cout << "#" << i + 1 << " " << 1 << endl;
+		else
+			cout << "#" << i + 1 << " " << 0 << endl;
+	}
+
+
 }
---
+++ Python/Algorithm/SWEA/1213. String.py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin = open('1213_input.txt', 'r')
+
+for tc in range(1, 11):
+    
