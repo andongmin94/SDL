cbc49fb3d3fd8727de01ca36e40e970eccfc08b2
안동민 <k1016h@naver.com>
2023-08-10 09:05:00 +0900

    push

--- C++/SWEA/1221. GNS.cpp
+++ C++/SWEA/1221. GNS.cpp
@@ -1,57 +1,57 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-string number[10] = { "ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN" };
-
-int main()
-{
-	int T;
-	cin >> T;
-	for (int i = 0; i < T; i++)
-	{
-		string s_tc, input;
-		int tc;
-		cin >> s_tc >> tc;
-
-		int cnt[10] = { 0, };
-		for (int j = 0; j < tc; j++)
-		{
-			cin >> input;
-			if (input == "ZRO")
-				cnt[0]++;
-			else if (input == "ONE")
-				cnt[1]++;
-			else if (input == "TWO")
-				cnt[2]++;
-			else if (input == "THR")
-				cnt[3]++;
-			else if (input == "FOR")
-				cnt[4]++;
-			else if (input == "FIV")
-				cnt[5]++;
-			else if (input == "SIX")
-				cnt[6]++;
-			else if (input == "SVN")
-				cnt[7]++;
-			else if (input == "EGT")
-				cnt[8]++;
-			else if (input == "NIN")
-				cnt[9]++;
-		}
-		cout << s_tc << endl;
-		for (int k = 0; k < 10; k++)
-		{
-			if (cnt[k] != 0)
-			{
-				for (int l = 0; l < cnt[k]; l++)
-					cout << number[k] << " ";
-			}
-		}
-		cout << endl;
-	}
-	return 0;
-}
+// #include <iostream>
+// #include <algorithm>
+// #include <string>
+// #include <vector>
+//
+// using namespace std;
+//
+// string number[10] = { "ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN" };
+//
+// int main()
+// {
+// 	int T;
+// 	cin >> T;
+// 	for (int i = 0; i < T; i++)
+// 	{
+// 		string s_tc, input;
+// 		int tc;
+// 		cin >> s_tc >> tc;
+//
+// 		int cnt[10] = { 0, };
+// 		for (int j = 0; j < tc; j++)
+// 		{
+// 			cin >> input;
+// 			if (input == "ZRO")
+// 				cnt[0]++;
+// 			else if (input == "ONE")
+// 				cnt[1]++;
+// 			else if (input == "TWO")
+// 				cnt[2]++;
+// 			else if (input == "THR")
+// 				cnt[3]++;
+// 			else if (input == "FOR")
+// 				cnt[4]++;
+// 			else if (input == "FIV")
+// 				cnt[5]++;
+// 			else if (input == "SIX")
+// 				cnt[6]++;
+// 			else if (input == "SVN")
+// 				cnt[7]++;
+// 			else if (input == "EGT")
+// 				cnt[8]++;
+// 			else if (input == "NIN")
+// 				cnt[9]++;
+// 		}
+// 		cout << s_tc << endl;
+// 		for (int k = 0; k < 10; k++)
+// 		{
+// 			if (cnt[k] != 0)
+// 			{
+// 				for (int l = 0; l < cnt[k]; l++)
+// 					cout << number[k] << " ";
+// 			}
+// 		}
+// 		cout << endl;
+// 	}
+// 	return 0;
+// }
