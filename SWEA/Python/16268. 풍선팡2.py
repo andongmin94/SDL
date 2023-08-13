96e142bb0e5c20e3ec6d774576fddf57c88df8a0
안동민 <k1016h@naver.com>
2023-08-13 23:03:48 +0900

    2023-08-13 오후 11:03

---
+++ SWEA/Python/16268. 풍선팡2.py
@@ -0,0 +1,21 @@
+﻿T = int(input())
+
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    array = [list(map(int, input().split())) for _ in range(N)]
+    di = [-1, 0, 1, 0]
+    dj = [0, 1, 0, -1]
+    max_r = 0
+
+    for i in range(N):
+        for j in range(M):
+            s = array[i][j]
+            for k in range(4):
+                    ni = i + di[k]
+                    nj = j + dj[k]
+                    if 0 <= ni < N and 0 <= nj < M:
+                        s += array[ni][nj]
+                    if max_r < s:
+                        max_r = s
+    print(f'#{tc} {max_r}')
+
--- SWEA/Python/16432. 오르막길 내리막길 2D HARD.py
+++ SWEA/Python/16432. 오르막길 내리막길 2D HARD.py
@@ -1,15 +1,14 @@
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    # 8방향 설정
     di = [-1, -1, -1, 0, 0, 1, 1, 1]
     dj = [-1, 0, 1, -1, 1, -1, 0, 1]
-
-    # 산의 정상에 해당하는지 검사하고 해당하면 체커에 추가
-    checker = []
+    
+    check = []
+    
     for i in range(1, N - 1):
         for j in range(1, N - 1):
             k = 0
@@ -19,15 +18,13 @@ for tc in range(1, T+1):
                 else:
                     break
             if k == 8:
-                checker.append(arr[i][j])
-    
-    # 산의 정상이 하나 이하이면 -1 출력
-    if len(checker) <= 1:
+                check.append(arr[i][j])
+                
+    if len(check) <= 1:
         print(f'#{tc} -1')
         continue
-        
-    # 최대값, 최소값
-    max_val = max(checker)
-    min_val = min(checker)
-    
+
+    max_val = max(check)
+    min_val = min(check)
+
     print(f'#{tc} {max_val - min_val}')
