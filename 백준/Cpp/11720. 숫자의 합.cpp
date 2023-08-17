8b7a7f46c17996f343e1ec8ac0205475621074f0
안동민 <k1016h@naver.com>
2023-08-17 23:27:55 +0900

    2023-08-17 오후 11:27

--- 백준/Cpp/11720. 숫자의 합.cpp
+++ 백준/Cpp/11720. 숫자의 합.cpp
@@ -1,17 +1,17 @@
-﻿// #include <iostream>
-//
-// using namespace std;
-//
-// int main()
-// {
-//     int T;
-//     cin >> T;
-//     char a;
-//     int sum = 0;
-//     for (int i = 0; i < T; i++)
-//     {
-//         cin >> a;
-//         sum += (int)(a - 48);
-//     }
-//     cout << sum;
-// }
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    char a;
+    int sum = 0;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> a;
+        sum += a - 48;
+    }
+    cout << sum;
+}
