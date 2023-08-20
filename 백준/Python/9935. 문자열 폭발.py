710b4d885411d28d8676edc9daf0bbd73aa1e322
안동민 <andongmin94@gmail.com>
2023-08-20 17:53:44 +0900

    2023-08-20 오후 05:53

---
+++ 백준/Python/9935. 문자열 폭발.py
@@ -0,0 +1,22 @@
+# try:
+#     T = list(input())
+#     P = input()
+#     p = len(P)
+#     i = 0
+#     while (True):
+#         j = 0
+#         for k in range(p):
+#             if T[i + k] == P[k]:
+#                 j += 1
+#         if j == p:
+#             for _ in range(p):
+#                 del(T[i])
+#             i = 0
+#             if T == []:
+#                 print("FRULA")
+#                 break
+#             continue
+#         i += 1
+# except:
+#     print(''.join(T))
+
