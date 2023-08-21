c1711ba06b598c42bdd1c5fd146a459a3a010523
안동민 <andongmin94@gmail.com>
2023-08-21 15:57:28 +0900

    seat change

--- 민코딩/main.cpp
+++ 민코딩/main.cpp
@@ -2,27 +2,24 @@
 
 using namespace std;
 
-
-int arr[5];
-int arr2[5];
-
-void PrintAll()
+int KFC(int* arr)
 {
-    for(int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
         cout << arr[i];
-    cout << endl;
-    for(int i = 0; i < 5; i++)
-        cout << arr2[i];
 }
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    for (int i = 0; i < 5; i++)
-        arr[i] = a;
-    for (int i = 0; i < 5; i++)
-        arr2[i] = b;
+    int arr[5];
 
-    PrintAll();
+    int oneminus;
+    cin >> oneminus;
+
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i] = oneminus;
+        oneminus -= 1;
+    }
+
+    KFC(arr);
 }
