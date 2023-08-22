4d491ff6ec0b0f59ab71927d3d834f3f958f45c7
안동민 <andongmin94@gmail.com>
2023-08-22 18:02:08 +0900

    2023-08-22 오후 06:02

---
+++ SWEA/Python/16681. subtree.py
@@ -0,0 +1,53 @@
+﻿'''
+T = int(input())
+for tc in range(1, T + 1):
+    E, N = map(int, input().split())
+    arr = list(map(int, input().split()))
+    stack = []
+    stack.append(N)
+    i = 0
+    while(i < len(stack)):
+        for j in range(E * 2):
+            if j % 2 == 0 and arr[j] == stack[i]:
+                stack.append(arr[j + 1])
+        i += 1
+
+    print(f'#{tc} {len(stack)}')
+'''
+def preorder(tree, p):
+    global cnt
+    if p == 0:
+        return
+    # 카운트를 1 증가
+    cnt += 1
+    # 자기자신에 대한 탐색
+    # 왼쪽 자식 탐색 진행
+    preorder(tree, tree[p][0])
+    # 오른쪽 자식 탐색 진행
+    preorder(tree, tree[p][1])
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    E, N = map(int, input().split())
+    nodes = list(map(int, input().split()))
+    
+    # 로직
+    # 자식 노드를 표기할 수 있는 이차원 배열 tree.
+    tree = [[0] * 2 for _ in range(E + 2)]
+    for i in range(0, len(nodes), 2):
+        # 부모 노드의 번호
+        p = nodes[i]
+        # 자식 노드의 번호
+        c = nodes[i+1]
+        # 부모 자식 관계를 부여...
+        if tree[p][0] == 0:
+            tree[p][0] = c # 왼쪽 자식
+        else:
+            tree[p][1] = c # 오른쪽 자식
+    
+    # N번 노드로 시작해서 순회를 하며, 서브트리 내에 있는 노드의 갯수를 카운트
+    cnt = 0
+    preorder(tree, N)
+    # 출력
+    print(f'#{tc} {cnt}')
---
+++ SWEA/Python/16703. 이진 힙.py
@@ -0,0 +1,45 @@
+﻿'''
+from heapq import heappop, heappush, heapify
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    heap = []
+    for i in range(len(arr)):
+        heappush(heap, arr[i])
+    tree = [0]
+    for i in range(len(heap)):
+        tree.append(heap[i])
+    grand_sum = 0
+    grand = len(tree) - 1
+    while grand > 0:
+        grand //= 2
+        grand_sum += tree[grand]
+    print(f'#{tc} {grand_sum}')
+'''
+from heapq import heappush
+
+T = int(input())
+for tc in range(1, T + 1):
+    # 입력
+    # 노드의 개수 N
+    N = int(input())
+    # 노드들의 값
+    nodes = list(map(int, input().split()))
+
+    # 로직
+    # 최소 힙에다가 N개의 노드들을 하나씩 삽입하여 힙을 완성시킨다...
+    heap = [] # 힙으로 사용할 빈 리스트...
+    for i in range(N):
+        heappush(heap, nodes[i])
+    heap = [0] + heap
+    # 출력
+    # 마지막 노드에서 루트 노드까지의 합을 출력한다.
+    total = 0
+    # 자식 인덱스 // 2 => 부모 인덱스
+    p = len(heap) - 1
+    while p > 0:
+        p = p // 2 # 부모 노드로 이동한다
+        total += heap[p] # 현재 노드의 값을 더해주고
+    print(f'#{tc} {total}')
