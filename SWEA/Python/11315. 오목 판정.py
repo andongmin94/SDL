1c27a79c5fd901ddb1895110beb973fd1500107e
안동민 <andongmin94@gmail.com>
2023-08-23 18:00:58 +0900

    update

---
+++ SWEA/Python/11315. 오목 판정.py
@@ -0,0 +1,23 @@
+﻿T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(input()) for _ in range(N)]
+    ans = -1
+    di = [0, 1, 1, 1, 0, -1, -1, -1]
+    dj = [1, 1, 0, -1, -1, -1, 0, 1]
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 'o':
+                for k in range(8):
+                    cnt = 0
+                    for l in range(5):
+                        ni, nj = i + di[k] * l, j + dj[k] * l
+                        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 'o':
+                            cnt +=1
+                    if cnt == 5:
+                        ans = 1
+
+    if ans != -1:
+        print(f'#{tc} YES')
+    else:
+        print(f'#{tc} NO')
---
+++ SWEA/Python/1926. 간단한 369게임.py
@@ -0,0 +1,7 @@
+﻿N = int(input())
+for i in range(1, N + 1):
+    clap = str(i).count('3') + str(i).count('6') + str(i).count('9')
+    if clap:
+        print('-' * clap, end = ' ')
+    else:
+        print(i, end = ' ')
