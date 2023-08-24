0ef961927631ae5992eb7a6050d4e7655321c96e
안동민 <andongmin94@gmail.com>
2023-08-25 00:56:46 +0900

    2023-08-25 오전 12:56

--- 싸피/Python/3 Stack/Stack 2-5) [참고] 부분집합의 합 1.md
+++ 싸피/Python/3 Stack/Stack 2-5) [참고] 부분집합의 합 1.md
@@ -7,6 +7,52 @@ f(i, N) # i : 현재 단계, N : 목표
         return
     else:
         f(i+1, N)
-
-
 ```
+```python
+# 재귀 1
+def f(i, N):
+    if i == N:
+        print(B)
+        return
+    else:
+        B[i] = A[i]
+        f(i+1,N)
+        return
+
+N = 3
+A = [1,2,3]
+B = [0] * N
+f(0, N)
+print(B)
+```
+```python
+# 부분집합 재귀
+def f(i, N):
+    if i == N:
+        print(bit, end = ' ')
+        for j in range(N):
+            if bit[j]:
+                print(A[j], end = ' ')
+        print()
+        return
+    else:
+        bit[i] = 1
+        f(i+1, N)
+        bit[i] = 0
+        f(i+1, N)
+        return
+
+A = [1,2,3]
+bit = [0] * 3
+f(0,3)
+```
+
+
+
+
+
+
+
+
+
+
--- 싸피/Python/test.py
+++ 싸피/Python/test.py
@@ -1,32 +1,24 @@
-# 재귀함수로 부분집합 만들기
-
-# bits : 해당 인덱스의 요소를 사용할지의 유무 (1:o, 0:x)
-# depth : 내가 얼마나 함수를 재귀호출했는지에 대한 카운트값..
-# current : 현재까지 부분 집합 요소들의 중간합
-def recur(bits, depth, current):
-    if current > 10:  # 가지치기
+# 부분집합의 합 연습문제
+def f(i, N, s, t):  # s : i - 1 원소까지 부분집합의 합(포함된 원소의 합), t : 찾으려는 합
+    global cnt
+    cnt += 1
+    if s == t: # 지금까지의 합이 찾는 목표치에 도달했는지 검사
+        print(bit)
+    elif i == N: # 남은 원소가 없는 경우
+        return
+    elif s > t:
+        return
+    else:
+        bit[i] = 1  # 부분 집합에 A[i] 포함
+        f(i + 1, N, s + A[i], t)
+        bit[i] = 0  # 부분 집합에 A[i] 빠짐
+        f(i + 1, N, s, t)
         return
 
-    # 기저조건
-    if depth == N:
-        if current == 10:
-            # 총합이 10이라면...
-            # 부분집합을 출력하는 코드
-            for i in range(N):
-                if bits[i] == 1:
-                    print(arr[i], end=' ')
-            print()
-        return
-
-    for i in range(2):  # 0, 1
-        bits[depth] = i
-        # 자기 자신을 호출
-        if bits[depth] == 1:
-            recur(bits, depth + 1, current + arr[i])
-        else:  # 0
-            recur(bits, depth + 1, current)
-
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-N = len(arr)
-recur([0] * N, 0, 0)
+# 1부터 10까지 원소인 집합, 부분집합의 합이 10인 경우는?
+N = 10
+A = [i for i in range(1, N + 1)]
+bit = [0] * N
+cnt = 0
+f(0, N, 0, 55)
+print(cnt)
