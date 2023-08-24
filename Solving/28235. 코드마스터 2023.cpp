8f5e5bb42316b6f4498fb41b4c7d326dff4780f2
안동민 <andongmin94@gmail.com>
2023-08-24 18:02:23 +0900

    2023-08-24 오후 06:02

---
+++ Solving/28235. 코드마스터 2023.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string input;
+    cin >> input;
+    cout << input;
+    if (strcmp(&input,"SONGDO"))
+        getchar();
+        cout << "HIGHSCHOOL";
+    // if (input == "CODE")
+    //     cout << "MASTER";
+    // if (input == "2023")
+    //     cout << "0611";
+    // if (input == "ALGORITHM")
+    //     cout << "CONTEST";
+
+}
