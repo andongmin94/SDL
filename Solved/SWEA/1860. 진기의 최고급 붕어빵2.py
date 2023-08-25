efe55fd94415044919018c40d386000934def766
안동민 <andongmin94@gmail.com>
2023-08-25 15:59:59 +0900

    2023-08-25 오후 03:59

---
+++ Solved/SWEA/1860. 진기의 최고급 붕어빵2.py
@@ -0,0 +1,11 @@
+﻿T = int(input())
+for tc in range(1, T + 1):
+    N, M, K = map(int, input().split())  # N 손님 수, M초마다 K개 생산
+    arr = list(map(int, input().split()))  # N명이 각각 도착하는 시간
+    arr.sort()  # 도착시간 순으로 정렬
+    result = 'Possible'
+    for i in range(N):
+        if i + 1 > arr[i] // M * K:
+            result = 'Impossible'
+            break
+    print(f'#{tc} {result}')
