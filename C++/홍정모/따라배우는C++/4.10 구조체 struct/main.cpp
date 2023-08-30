45562abe75e5efb16f7484b12f0724f78f6b68da
안동민 <andongmin94@gmail.com>
2023-08-30 23:47:29 +0900

    2023-08-30 오후 11:47

--- C++/홍정모/따라배우는C++/4.10 구조체 struct/main.cpp
+++ C++/홍정모/따라배우는C++/4.10 구조체 struct/main.cpp
@@ -3,21 +3,29 @@
 
 using namespace std;
 
+struct Person
+{
+	double height;
+	float  weight;
+	int	   age;
+	string name;
+
+	void print()
+	{
+		cout << height << " " << weight << " " << age << " " << name;
+		cout << endl;
+	}
+};
+
+struct Family
+{
+	Person me, mom, dad;
+};
+
 int main()
 {
-    string s;
-    cin >> s;
-    string ALPHA = "abcdefghijklmnopqrstuvwxyz";
-    int arr[26] = { 0, };
-
-    for (int i = 0; i < 26; i++)
-        arr[i] = -1;
-
-    for (int k = 0; k < s.length(); k++)
-        for (int j = 0; j < ALPHA.length(); j++)
-            if (ALPHA[j] == s[k] && arr[j] == -1)
-                arr[j] = k;
-
-    for (int i = 0; i < 26; i++)
-        cout << arr[i] << " ";
+	Person me{ 2.0, 100.0, 20, "Jack Jack" };
+	Person me2;
+	me2 = me;
+	me2.print();
 }
