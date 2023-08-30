6693898c494cbb45e5813aec0260547c05815621
안동민 <andongmin94@gmail.com>
2023-08-30 17:57:44 +0900

    2023-08-30 오후 05:57

---
+++ C++/TEMP/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[6] = { 3,4,2,5,7,9 };
+}
--- WEB/이론/웹_파이썬/test.py
+++ WEB/이론/웹_파이썬/test.py
@@ -1,24 +1,31 @@
-'''
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input()) 
-    arr = [list(map(int, input().split())) for _ in range(N)]
- 
-    cnt = [0] * 201
- 
-    for a, b in arr:  
-        a = (a+a%2)//2
-        b = (b+b%2)//2
-        for i in range(min(a,b), max(a,b) + 1):
-            cnt[i] += 1
- 
-    print(f'#{tc} {max(cnt)}')
-'''
 T = int(input())
 for tc in range(1, T + 1):
-    print(f'#{tc}', end = ' ')
-    N = int(input())
-    room = [0] * 200
-    
-    for i in range(N):
-        st, en = map(int, input().split())
+    arr = list(map(int, input().strip()))
+    c = [0] * 12
+    check = 0
+    player = [arr[0],arr[1],arr[2],arr[3],arr[4],arr[5]]
+    for i in range(6):
+        c[player[i]] += 1
+    k = 0
+    while (k < 10):
+        if c[k] >= 3:
+            c[k] -= 3
+            check += 1
+            k -= 1
+            if check >= 2:
+                print(f'#{tc} true')
+                break
+            continue
+        if c[k] >= 1 and c[k + 1] >= 1 and c[k + 2] >= 1:
+            c[k] -= 1
+            c[k + 1] -= 1
+            c[k + 2] -= 1
+            check += 1
+            k -= 1
+            if check >= 2:
+                print(f'#{tc} true')
+                break
+            continue
+        k += 1
+    if check != 2:
+        print(f'#{tc} false')
