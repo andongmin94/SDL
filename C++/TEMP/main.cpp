b2cc1f59692a55ca00ecc64da302499ff7e49d62
안동민 <andongmin94@gmail.com>
2023-08-31 17:58:38 +0900

    Update main.cpp

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,24 +2,18 @@
 
 using namespace std;
 
+int a, b;
+
+void BBQ()
+{
+	cout << "ÇÕ:" << a + b << endl;
+	cout << "Â÷:" << a - b << endl;
+	cout << "°ö:" << a * b << endl;
+	cout << "¸ò:" << a / b << endl;
+}
+
 int main()
 {
-	int arr[6][2];
-	int cnt = 0;
-	for (int i = 0; i < 6; i++)
-	{
-		for (int j = 0; j < 2; j++)
-			cin >> arr[i][j];
-		if (arr[i][0] < arr[i][1])
-		{
-			int temp = arr[i][0];
-			arr[i][0] = arr[i][1];
-			arr[i][1] = temp;
-			cnt++;
-		}
-		for (int j = 0; j < 2; j++)
-			cout << arr[i][j] << " ";
-		cout << endl;
-	}
-	cout << cnt << "¸í";
+	cin >> a >> b;
+	BBQ();
 }
