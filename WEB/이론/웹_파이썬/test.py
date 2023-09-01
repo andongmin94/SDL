9262194ba17fa64d12d185ada4dd78da732d6209
안동민 <andongmin94@gmail.com>
2023-09-01 17:41:02 +0900

    2023-09-01 오후 05:40

--- WEB/이론/웹_파이썬/test.py
+++ WEB/이론/웹_파이썬/test.py
@@ -1,30 +1,14 @@
-# 현강
-
-# subset_test
-# 부분집합 만들기
-# 해당 원소를 사용하면 1, 해당 원소를 사용하지 않으면 0
-# N : 배열의 길이, i : 재귀호출한 깊이
-def subset(N, i, s):
-    # 기저조건
-    if i == N:
-        if s == 0:
-            print(*result)
-        return
-
-    # i 번째에 있는 요소를 사용
-    # bits[i] = 1
-    result.append(arr[i])
-    subset(N, i + 1, s + arr[i])
-
-    # i 번째에 있는 요소를 사용 x
-    # bits[i] = 0
-    result.pop()
-    subset(N, i + 1, s)
-
-
-arr = [-1,3,-9,6,7,-6,1,5,4,-2]
-N = len(arr)
-bits = [0] * N
-s = 0
-result = []
-subset(N, 0, s)
+T = int(input())
+for tc in range(1, T + 1):
+    bin = input()
+    tri = list(input())
+    bin_set = set()
+    tri_set = set()
+    for i in range(len(bin)):
+        bin_set.add(int(bin, 2) ^ 2 ** i)
+    for i in range(len(tri)):
+        for j in {'0', '1', '2'} - {tri[i]}:
+            tmp = tri[:]
+            tmp[i] = j
+            tri_set.add(int(''.join(tmp), 3))
+    print(f'#{tc}', *(bin_set & tri_set))
