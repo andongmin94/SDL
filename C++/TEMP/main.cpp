a2e2c5e7233a00f1209316137753b101ff4c006b
안동민 <andongmin94@gmail.com>
2023-09-05 12:14:21 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
-char arr[2][3] = { 'F','E','W','D','C','A' };
-
-bool findCh(char target)
-{
-	bool count = false;
-	for (int i = 0; i < 2; i++)
-		for (int j = 0; j < 3; j++)
-			if (arr[i][j] == target)
-				count = true;
-	return count;
-}	
-
 int main()
 {
-	char a;
-	cin >> a;
-	if (findCh(a))
-		cout << "발견";
-	else
-		cout << "미발견";
+	int arr[10];
+	set<int> res;
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> arr[i];
+		arr[i] %= 42;
+		res.insert(arr[i]);
+	}
+	cout << res.size();
 }
