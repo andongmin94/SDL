e792f00be41b99f322794648fa4a127bf718ad5b
안동민 <andongmin94@gmail.com>
2023-09-07 17:29:37 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,56 +2,22 @@
 
 using namespace std;
 
+char arr[3][5] = { 'D','A','C','C','D','S','D','F','A','E','E','E','T','J','H' };
+
+int CHECK(char a)
+{
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 5; j++)
+			if (arr[i][j] == a)
+				return 1;
+	return 0;
+}
+
 int main()
 {
-	int a, arr[3][3];
+	char a;
 	cin >> a;
-	if (a % 5 == 1)
-	{
-		int k = 1;
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
-			{
-				arr[3 - j][3 - i] = k;
-				k++;
-			}
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-				cout << arr[i][j] << " ";
-			cout << endl;
-		}
-	}
-	else if (a % 5 == 2)
-	{
-		int k = 1;
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
-			{
-				arr[2 - i][j] = k;
-				k++;
-			}
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-				cout << arr[i][j] << " ";
-			cout << endl;
-		}
-	}
-	else
-	{
-		int k = 10;
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
-			{
-				arr[j][i] = k;
-				k++;
-			}
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-				cout << arr[i][j] << " ";
-			cout << endl;
-		}
-	}
+	if (CHECK(a) == 1)
+		cout << "있음";
+	else cout << "없음";
 }
