d420259eecf072ebc86adf4b9360e8a844b4d58e
안동민 <andongmin94@gmail.com>
2023-09-07 15:24:51 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,32 +2,56 @@
 
 using namespace std;
 
-void BBQ(int n)
-{
-	for (int i = 0; i < n; i++)
-		cout << i + 1;
-}
-
-void KFC(char b)
-{
-	for (int i = 0; i < 7; i++)
-		cout << b;
-}
-
 int main()
 {
-	int a;
+	int a, arr[3][3];
 	cin >> a;
-	if (a % 2 != 0)
+	if (a % 5 == 1)
 	{
-		int b;
-		cin >> b;
-		BBQ(b);
+		int k = 1;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+			{
+				arr[3 - j][3 - i] = k;
+				k++;
+			}
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+				cout << arr[i][j] << " ";
+			cout << endl;
+		}
+	}
+	else if (a % 5 == 2)
+	{
+		int k = 1;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+			{
+				arr[2 - i][j] = k;
+				k++;
+			}
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+				cout << arr[i][j] << " ";
+			cout << endl;
+		}
 	}
 	else
 	{
-		char b;
-		cin >> b;
-		KFC(b);
+		int k = 10;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+			{
+				arr[j][i] = k;
+				k++;
+			}
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+				cout << arr[i][j] << " ";
+			cout << endl;
+		}
 	}
 }
