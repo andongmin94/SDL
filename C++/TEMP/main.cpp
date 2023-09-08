761992b65f3a22e325162035f2b0d63aa5a2242f
안동민 <andongmin94@gmail.com>
2023-09-08 16:49:08 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,22 +2,11 @@
 
 using namespace std;
 
-char arr[3][5] = { 'D','A','C','C','D','S','D','F','A','E','E','E','T','J','H' };
-
-int CHECK(char a)
-{
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 5; j++)
-			if (arr[i][j] == a)
-				return 1;
-	return 0;
-}
-
 int main()
 {
-	char a;
-	cin >> a;
-	if (CHECK(a) == 1)
-		cout << "있음";
-	else cout << "없음";
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a * b - c >= 0)
+		cout << a * b - c;
+	else cout << 0;
 }
