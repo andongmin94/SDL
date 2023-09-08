06304a656e06781096a5b5f882fd9086db2ac5a2
안동민 <andongmin94@gmail.com>
2023-09-08 17:08:10 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -4,9 +4,34 @@ using namespace std;
 
 int main()
 {
+	int arr[3][4];
 	int a, b, c;
 	cin >> a >> b >> c;
-	if (a * b - c >= 0)
-		cout << a * b - c;
-	else cout << 0;
+	for (int i = 0; i < 3; i++)
+	{
+		if (i == 0)
+			for (int j = 0; j < 4; j++)
+			{
+				arr[i][j] = a;
+				a++;
+			}
+		if (i == 1)
+			for (int j = 0; j < 4; j++)
+			{
+				arr[i][j] = b;
+				b++;
+			}
+		if (i == 2)
+			for (int j = 0; j < 4; j++)
+			{
+				arr[i][j] = c;
+				c++;
+			}
+	}
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 4; j++)
+				cout << arr[i][j] << " ";
+			cout << endl;
+		}
 }
