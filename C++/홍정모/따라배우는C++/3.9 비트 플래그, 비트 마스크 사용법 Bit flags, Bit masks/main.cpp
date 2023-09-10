74fe344d5b3740d6f035e85330fb4a3987b7374f
안동민 <andongmin94@gmail.com>
2023-09-11 02:53:03 +0900

    3.

--- C++/홍정모/따라배우는C++/3.9 비트 플래그, 비트 마스크 사용법 Bit flags, Bit masks/main.cpp
+++ C++/홍정모/따라배우는C++/3.9 비트 플래그, 비트 마스크 사용법 Bit flags, Bit masks/main.cpp
@@ -4,5 +4,19 @@ using namespace std;
 
 int main()
 {
+	bool item1_flag = false;
+	bool item2_flag = true;
+	bool item3_flag = false;
+	bool item4_flag = false;
 
+	// event!
+	item1_flag = true;
+
+	// die! item2 los
+	item2_flag = false;
+
+	if (item3_flag == true)
+	{
+		// event
+	}
 }
