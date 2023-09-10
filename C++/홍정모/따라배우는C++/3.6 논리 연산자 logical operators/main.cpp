b8bb1cadb81343e8e2fc75964f706336d43c76cd
안동민 <andongmin94@gmail.com>
2023-09-11 02:53:03 +0900

    3.

--- C++/홍정모/따라배우는C++/3.6 논리 연산자 logical operators/main.cpp
+++ C++/홍정모/따라배우는C++/3.6 논리 연산자 logical operators/main.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
+	int x = 2;
+	int y = 2;
 
+	if (y++ == 2 && x == 2)
+	{
+		int z = 3;
+	}
+	cout << y;
 }
--- C++/홍정모/따라배우는C++/3.8 비트단위 연산자 Bitwise Operators/main.cpp
+++ C++/홍정모/따라배우는C++/3.8 비트단위 연산자 Bitwise Operators/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
 int main()
 {
+	// << left shift
+	// >> right shift
+	// ~, &, |, ^
 
+	//int a = 3;
+
+	//cout << std::bitset<8>(a) << " " << a <<endl;
+
+	//cout << std::bitset<8>(a << 1) << " " << (a << 1) << endl;
+	//cout << std::bitset<8>(a << 2) << " " << (a << 2) << endl;
+	//cout << std::bitset<8>(a << 3) << " " << (a << 3) << endl;
+
+	//int a = 1024;
+
+	//cout << std::bitset<16>(a) << endl;
+	//cout << std::bitset<16>(~a) << " " << (~a) << endl;
+
+	int a = 0b1100;
+	int b = 0b0110;
+
+	//cout << a << " " << b << endl;
+	cout << std::bitset<4>(a & b) << endl;
+	cout << std::bitset<4>(a | b) << endl;
+	cout << std::bitset<4>(a ^ b) << endl;
+
+	a &= b;
+	cout << a;
 }
