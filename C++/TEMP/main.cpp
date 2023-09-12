e878f699dbff218e0a415496e402855516960c54
안동민 <andongmin94@gmail.com>
2023-09-12 17:52:31 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,19 +2,18 @@
 
 using namespace std;
 
-struct BBQ
+int yesOrNo()
 {
-	int x;
-	int data[3];
-};
+	int a;
+	cin >> a;
+	if (a % 3 == 0)
+		return 7;
+	else if (a % 3 == 1)
+		return 35;
+	else return 50;
+}
 
 int main()
 {
-	BBQ g;
-	cin >> g.x;
-	cin >> g.data[0] >> g.data[1] >> g.data[2];
-	int sum = 0;
-	for (int i = 0; i < 3; i++)
-		sum += g.data[i];
-	cout << sum << " " << g.x;
+	cout << yesOrNo();
 }
