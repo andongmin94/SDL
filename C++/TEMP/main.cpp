59fa5b9edbe12da2d290b40097f0bf31b5c86dde
안동민 <andongmin94@gmail.com>
2023-09-15 17:54:40 +0900

    Update main.cpp

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,21 +2,12 @@
 
 using namespace std;
 
+struct Node
+{
+
+};
+
 int main()
 {
-	int a;
-	char b;
-	cin >> a >> b;
-	int arr[5][5] = { 0, };
-	for (int i = 0; i < 5; i++)
-		arr[a - 1][4 - i] = b++;
 
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-			if (arr[i][j] == 0)
-				cout << 0;
-			else cout << (char)arr[i][j];
-		cout << endl;
-	}
 }
