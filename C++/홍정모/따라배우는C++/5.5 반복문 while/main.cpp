c9688f968f2370824c4aa6988d3d2aab712e641e
안동민 <andongmin94@gmail.com>
2023-09-18 20:41:55 +0900

    .

--- C++/홍정모/따라배우는C++/5.5 반복문 while/main.cpp
+++ C++/홍정모/따라배우는C++/5.5 반복문 while/main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main()
 {
+	cout << "while-loop test" << endl;
+	
+	int outer_count = 1;
 
+	while (outer_count <= 5)
+	{
+		int inner_count = 1;
+		while (inner_count <= outer_count)
+		{
+			cout << inner_count++ << " ";
+		}
+		cout << endl;
+		++outer_count;
+	}
 }
