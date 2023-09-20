45680ba6186aad25c538fd30a80b7d061974b346
안동민 <andongmin94@gmail.com>
2023-09-20 17:58:21 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,18 +2,32 @@
 
 using namespace std;
 
+int arr[4][4] = { 3,4,1,6,3,5,3,6,0,0,0,0,5,4,6,0 };
+
 int main()
 {
-	int arr[3][3] = { 0, };
-	int a;
-	cin >> a;
-	for (int i = 0; i < 3; i++)
-		for (int j = 2 - i; j < 3; j++)
-				arr[i][j] = a++;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-			cout << arr[i][j];
-		cout << endl;
-	}
+	for (int i = 0; i < 4; i++)
+		cin >> arr[2][i];
+	int max = 0;
+	int min = 10;
+	int max_i, max_j;
+	int min_i, min_j;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
+		{
+			if (max < arr[i][j])
+			{
+				max = arr[i][j];
+				max_i = i;
+				max_j = j;
+			}
+			else if (min > arr[i][j])
+			{
+				min = arr[i][j];
+				min_i = i;
+				min_j = j;
+			}
+		}
+	cout << "MAX=" << max << "(" << max_i << "," << max_j << ")" << endl;
+	cout << "MIN=" << min << "(" << min_i << "," << min_j << ")" << endl;
 }
