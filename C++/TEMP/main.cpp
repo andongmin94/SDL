40be73abe019461a322f5990cb45b97b6de982fa
안동민 <andongmin94@gmail.com>
2023-09-21 18:01:21 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
-char arr[2][6] = { '4', '5','7','1','3','2','D','F','Q','W','G','Z' };
-
 int main()
 {
+	char arr1[10];
+	char arr2[10];
+	char arr3[10];
+	cin >> arr1 >> arr2 >> arr3;
 
+	bool check = true;
+	for (int i = 0; arr1[i] != 0; i++)
+		if (arr1[i] != arr2[i] || arr2[i] != arr3[i])
+			check = false;
+	if (check == true)
+		cout << "YES";
+	else cout << "NO";
 }
