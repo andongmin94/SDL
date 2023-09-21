356439cb1dee512803fbd871e0c470b748538b89
안동민 <andongmin94@gmail.com>
2023-09-22 00:28:24 +0900

    .

--- C++/홍정모/따라배우는C++/6.1 배열 기초 [1 of 2] array/main.cpp
+++ C++/홍정모/따라배우는C++/6.1 배열 기초 [1 of 2] array/main.cpp
@@ -2,26 +2,22 @@
 
 using namespace std;
 
+enum student_scores
+{
+	JACKJACK,
+	DASH,
+	violet,
+	NUM_STUDENTS,
+};
+
 int main()
 {
-	int one_student_score; // 1 variable
-	int student_scores[5]; // 5 int
-	
-	cout << sizeof(one_student_score) << endl;
-	cout << sizeof(student_scores) << endl;
+	int student_scores[NUM_STUDENTS];
 
-	one_student_score = 100;
-	student_scores[0] = 100; // 1st element
-	student_scores[1] = 80;	 // 2nd element
-	student_scores[2] = 90;  // 3rd element
-	student_scores[3] = 50;  // 4th element
-	student_scores[4] = 0;   // 5tm element
+	student_scores[JACKJACK] = 0;
+	student_scores[DASH] = 100;
 
+	const int num_students = 5;
 
-	cout << student_scores[0] << endl;
-	cout << student_scores[1] << endl;
-	cout << student_scores[2] << endl;
-	cout << student_scores[3] << endl;
-	cout << student_scores[4] << endl;
-
+	int arr[num_students];
 }
--- C++/홍정모/따라배우는C++/6.2 배열 기초 [2 of 2] array/main.cpp
+++ C++/홍정모/따라배우는C++/6.2 배열 기초 [2 of 2] array/main.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+#define NUM_STUDENTS 100000
+
+void doSomething(int arr[])
+{
+	cout << &arr << endl;
+	cout << &arr[1] << endl;
+	cout << arr[2] << endl;
+	cout << sizeof(arr) << endl;
+}
+
 int main()
 {
+	const int num_students = 5;
 
+	int arr[num_students] = { 1,2,3 };
+
+	cout << &arr << endl;
+	cout << &arr[1] << endl;
+	cout << arr[2] << endl;
+	cout << sizeof(arr) << endl;
+	doSomething(arr);
 }
