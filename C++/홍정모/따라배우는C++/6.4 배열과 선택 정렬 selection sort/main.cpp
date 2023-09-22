0732a9f2a6ebe1d1883b457334a026c3f4ba7e74
안동민 <andongmin94@gmail.com>
2023-09-22 17:53:34 +0900

    .

--- C++/홍정모/따라배우는C++/6.4 배열과 선택 정렬 selection sort/main.cpp
+++ C++/홍정모/따라배우는C++/6.4 배열과 선택 정렬 selection sort/main.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
+void printArray(const int array[], const int length)
+{
+	for (int index = 0; index < length; index++)
+		cout << array[index] << " ";
+	cout << endl;
+}
+
 int main()
 {
+	/*				value	index
+	3 5 2 1 4		  1		   3
+	1 5 2 3 4		  2		   2
+	1 2 5 3 4		  3		   3
+	1 2 3 5 4		  4		   4
+	1 2 3 4 5		  		   
+	*/
 
+	const int length = 5;
+
+	int array[length] = { 3,5,2,1,4 };
+	printArray(array, length);
+
+	// swap
+	int temp = array[0];
+	array[0] = array[1];
+	array[1] = temp;
+
+	printArray(array, length);
 }
