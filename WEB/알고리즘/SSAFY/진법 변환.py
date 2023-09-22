c9833e52f740cc03f407013ccef75a6259df593b
안동민 <andongmin94@gmail.com>
2023-09-22 22:22:36 +0900

    .

---
+++ WEB/알고리즘/SSAFY/진법 변환.py
@@ -0,0 +1,24 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    P = list(input())
+    Key = input()
+
+    if 65 <= ord(Key) <= 70:
+        Key = ord(Key) - 55
+    elif 48 <= ord(Key) <= 57:
+        Key = ord(Key) - 48
+
+    for i in range(N):
+        if 65 <= ord(P[i]) <= 70:
+            P[i] = (ord(P[i]) - 55) ^ Key
+        elif 48 <= ord(P[i]) <= 57:
+            P[i] = (ord(P[i]) - 48) ^ Key
+
+    for i in range(N):
+        if 10 <= P[i] <= 15:
+            P[i] = chr(P[i] + 55)
+        elif 0 <= P[i] <= 9:
+            P[i] = chr(P[i] + 48)
+    print(f'#{tc}', end=' ')
+    print(''.join(P))
