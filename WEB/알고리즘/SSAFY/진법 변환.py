100483565e5638c3620ef97088dab56de0b31987
안동민 <andongmin94@gmail.com>
2023-09-23 13:40:15 +0900

    .

--- WEB/알고리즘/SSAFY/진법 변환.py
+++ WEB/알고리즘/SSAFY/진법 변환.py
@@ -4,21 +4,43 @@ for tc in range(1, T + 1):
     P = list(input())
     Key = input()
 
-    if 65 <= ord(Key) <= 70:
-        Key = ord(Key) - 55
-    elif 48 <= ord(Key) <= 57:
-        Key = ord(Key) - 48
+    for i in range(N):
+        P[i] = int(P[i], 16)
+    Key = int(Key, 16)
 
     for i in range(N):
-        if 65 <= ord(P[i]) <= 70:
-            P[i] = (ord(P[i]) - 55) ^ Key
-        elif 48 <= ord(P[i]) <= 57:
-            P[i] = (ord(P[i]) - 48) ^ Key
+        P[i] = P[i] ^ Key
 
     for i in range(N):
         if 10 <= P[i] <= 15:
             P[i] = chr(P[i] + 55)
         elif 0 <= P[i] <= 9:
             P[i] = chr(P[i] + 48)
+
     print(f'#{tc}', end=' ')
     print(''.join(P))
+
+# T = int(input())
+# for tc in range(1, T + 1):
+#     N = int(input())
+#     P = list(input())
+#     Key = input()
+# 
+#     if 65 <= ord(Key) <= 70:
+#         Key = ord(Key) - 55
+#     elif 48 <= ord(Key) <= 57:
+#         Key = ord(Key) - 48
+# 
+#     for i in range(N):
+#         if 65 <= ord(P[i]) <= 70:
+#             P[i] = (ord(P[i]) - 55) ^ Key
+#         elif 48 <= ord(P[i]) <= 57:
+#             P[i] = (ord(P[i]) - 48) ^ Key
+# 
+#     for i in range(N):
+#         if 10 <= P[i] <= 15:
+#             P[i] = chr(P[i] + 55)
+#         elif 0 <= P[i] <= 9:
+#             P[i] = chr(P[i] + 48)
+#     print(f'#{tc}', end=' ')
+#     print(''.join(P))
