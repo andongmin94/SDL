c5050fa71e1610b2e1b6a20c39af01d31053b337
안동민 <andongmin94@gmail.com>
2023-09-25 17:57:36 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -4,21 +4,13 @@ using namespace std;
 
 int main()
 {
-    char arr[10];
-    cin >> arr;
-    int range = 0;
-    int stack = 0;
-    for (int i = 0; arr[i] != 0; i++)
+    int a, b, c;
+    cin >> a >> b >> c;
+    int logic = a + b;
+    for (int i = 0; i < c; i++)
     {
-        if (i % 2 == 0 && arr[i] >= 'A' && arr[i] <= 'Z')
-            stack++;
-        if (i % 2 != 0 && arr[i] >= 'a' && arr[i] <= 'z')
-            stack++;
-        range++;
+        for (int j = a; j < logic; j++)
+            cout << j << " ";
+        cout << endl;
     }
-    if (range == stack)
-    {
-        cout << "개구리문장";
-    }
-    else cout << "일반문장";
 }
