a0382121981fa98e9340770c50d304a7690e5c16
안동민 <andongmin94@gmail.com>
2023-09-27 13:40:10 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -4,9 +4,23 @@ using namespace std;
 
 int main()
 {
-	char arr[4][5] = { "ABKT","KFCF","BBQQ","TPZF" };
-	char a, b;
-	cin >> a >> b;
-	int cnt_a = 0;
-	int cnt_b = 0;
+	int n = 1;
+	int arr[7][4] = { 0, };
+	for (int i = 0; i < 7; i++)
+		for (int j = 0; j < 4; j++)
+			arr[i][j] = n++;
+	int a, b, c;
+	cin >> a >> b >> c;
+	for (int i = 0; i < 4; i++)
+	{
+		arr[a][i] = 0;
+		arr[b][i] = 0;
+		arr[c][i] = 0;
+	}
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 4; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}
 }
