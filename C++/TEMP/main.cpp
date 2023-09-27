658c92ee4f7e328ca2e6bb1869bd0a86b3f53f1f
안동민 <andongmin94@gmail.com>
2023-09-27 13:40:27 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -2,25 +2,28 @@
 
 using namespace std;
 
+void isExist(char (*arr)[7], char a, char b)
+{
+	bool a_check = false;
+	bool b_check = false;
+
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 6; j++)
+		{
+			if (arr[i][j] == a)
+				a_check = true;
+			if (arr[i][j] == b)
+				a_check = true;
+		}
+	if (a_check == true)
+		cout << a << " : 존재" << endl;
+	else cout << a << " : 없음" << endl;
+}
+
 int main()
 {
-	int n = 1;
-	int arr[7][4] = { 0, };
-	for (int i = 0; i < 7; i++)
-		for (int j = 0; j < 4; j++)
-			arr[i][j] = n++;
-	int a, b, c;
-	cin >> a >> b >> c;
-	for (int i = 0; i < 4; i++)
-	{
-		arr[a][i] = 0;
-		arr[b][i] = 0;
-		arr[c][i] = 0;
-	}
-	for (int i = 0; i < 7; i++)
-	{
-		for (int j = 0; j < 4; j++)
-			cout << arr[i][j] << " ";
-		cout << endl;
-	}
+	char arr2[2][7] = { "A79TKQ","MINCOD" };
+	char a, b;
+	cin >> a >> b;
+	
 }
