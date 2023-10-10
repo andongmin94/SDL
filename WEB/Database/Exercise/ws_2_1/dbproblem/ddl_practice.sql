bc4bb786d1073491f0d9b0421e70f62674c9d1f9
andongmin94 <andongmin94@gmail.com>
2023-10-10 18:04:07 +0900

    .

---
+++ WEB/Database/Exercise/ws_2_1/dbproblem/ddl_practice.sql
@@ -0,0 +1,10 @@
+CREATE TABLE animals (
+  animal_name TEXT NOT NULL,
+  height INTEGER NOT NULL,
+  weight INTEGER NOT NULL,
+  age INTEGER
+);
+
+ALTER TABLE animals RENAME COLUMN animal_name TO name;
+
+ALTER TABLE animals RENAME TO zoo;
