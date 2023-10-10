432d5aebb76913303cb866c354bd2499e127fd54
안동민 <andongmin94@gmail.com>
2023-10-11 01:36:31 +0900

    .

---
+++ C++/홍정모/따라배우는C++/6.15 참조와 const/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void doSomething(const int& x)
+{
+	cout << x << endl;
+}
+
+int main()
+{
+	int x = 5;
+	int& ref_x = x;
+
+	const int& ref_2 = ref_x;
+
+	const int& ref = 3 + 4;
+
+	cout << ref << endl;
+	cout << &ref << endl;
+	int a = 1;
+	doSomething(a);
+	doSomething(1);
+	doSomething(a + 1);
+}
