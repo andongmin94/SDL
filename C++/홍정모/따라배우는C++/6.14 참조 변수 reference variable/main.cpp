0e6f24e8b96d3b9deac689d3b8c9554faadf96fc
안동민 <andongmin94@gmail.com>
2023-10-11 01:36:31 +0900

    .

--- C++/홍정모/따라배우는C++/6.14 참조 변수 reference variable/main.cpp
+++ C++/홍정모/따라배우는C++/6.14 참조 변수 reference variable/main.cpp
@@ -2,18 +2,25 @@
 
 using namespace std;
 
+struct Something
+{
+	int v1;
+	float v2;
+};
+
+struct Other
+{
+	Something st;
+};
+
 int main()
 {
 	int value = 5;
 
-	//int* ptr = nullptr;
-	//ptr = &value;
-
+	// 같음
+	int* const ptr = &value;
 	int& ref = value;
 
-	cout << ref << endl;
-
+	*ptr = 10;
 	ref = 10;
-
-	cout << value << " " << ref << endl;
 }
---
+++ C++/홍정모/따라배우는C++/6.16 포인터와 참조의 멤버 선택/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Person
+{
+	int age;
+	double weight;
+};
+
+int main()
+{
+	Person person;
+	person.age = 5;
+	person.weight = 30;
+
+	Person& ref = person;
+	ref.age = 15;
+	ref.weight = 90;
+
+	Person* ptr = &person;
+	ptr->age = 30;
+	ptr->weight = 180;
+	(*ptr).age = 20;
+	(*ptr).weight = 120;
+
+	Person& ref2 = *ptr;
+	ref2.age = 45;
+	ref2.weight = 240;
+
+	cout << &person << endl;
+	cout << &ref2 << endl;
+}
