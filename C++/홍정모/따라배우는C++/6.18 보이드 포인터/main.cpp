d3e3df8bcd3807b69bf51fb4a0901146a24ec5aa
안동민 <andongmin94@gmail.com>
2023-10-11 23:22:26 +0900

    .

---
+++ C++/홍정모/따라배우는C++/6.18 보이드 포인터/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// void pointer, generic pointer
+
+enum Type
+{
+	INT,
+	FLOAT,
+	CHAR
+};
+
+int main()
+{
+	int i = 5;
+	float f = 3.0;
+	char c = 'a';
+
+	void* ptr = nullptr;
+
+	ptr = &i;
+	ptr = &f;
+	// ptr = &c;
+
+	Type type = FLOAT;
+	if (type == FLOAT)
+		cout << *static_cast<float*>(ptr) << endl;
+	else if (type == INT)
+		cout << *static_cast<float*>(ptr) << endl;
+
+	cout << &f << " " << ptr << endl;
+	cout << *static_cast<float*>(ptr) << endl;
+
+	int* ptr_i = &i;
+
+	cout << ptr_i << endl;
+	cout << ptr_i + 1<< endl;
+}
