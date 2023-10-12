62db5e954923cbe11d13179300ef61418eccded6
안동민 <andongmin94@gmail.com>
2023-10-13 03:13:06 +0900

    .

---
+++ C++/홍정모/따라배우는C++/6.21 vector 소개/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	// array<int, 5> array;
+	// 벡터는 동적 할당이라 사이즈를 안 써줌
+	vector<int> array;
+
+	vector<int> array2 = { 1,2,3,4,5 };
+
+	cout << array2.size() << endl;
+
+	vector<int> array3{ 1,2,3 };
+
+	cout << array3.size() << endl;
+
+	vector<int> arr = { 1,2,3,4,5 };
+	for (auto& itr : arr)
+		cout << itr << " ";
+	cout << endl;
+
+	arr.resize(10);
+	for (auto& itr : arr)
+		cout << itr << " ";
+	cout << endl;
+
+	cout << arr[1] << endl;
+	cout << arr.at(1) << endl;
+
+	cout << arr.size() << endl;
+
+	int* my_arr = new int[5];
+	delete my_arr;
+}
---
+++ C++/홍정모/따라배우는C++/7.1 매개변수와 실인자의 구분/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int foo(int x, int y);
+
+int foo(int x, int y)
+{
+	return x + y;
+} // x and y are destroyed here
+
+int main()
+{
+	int x = 1, y = 2;
+	foo(6, 7);
+	foo(x, y + 1);
+}
