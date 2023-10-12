8e0dc043ad9f28b3bb7620114e420339515c2775
안동민 <andongmin94@gmail.com>
2023-10-13 03:13:06 +0900

    .

--- C++/홍정모/따라배우는C++/6.20 array 소개/main.cpp
+++ C++/홍정모/따라배우는C++/6.20 array 소개/main.cpp
@@ -1,9 +1,26 @@
 ﻿#include <iostream>
 #include <array>
+#include <algorithm>
 
 using namespace std;
 
+void printLength(const array<int, 5>& my_arr)
+{
+	cout << my_arr.size() << endl;
+}
+
 int main()
 {
-	array<int, 5> my_arr = { 1,2,3,4,5 };
+	array<int, 5> my_arr = { 1,22,3,40,5 };
+	
+	std::sort(my_arr.begin(), my_arr.end());
+	for (auto element : my_arr)
+		cout << element << " ";
+
+	cout << endl;
+	// 역순
+	std::sort(my_arr.rbegin(), my_arr.rend());
+
+	for (auto element : my_arr)
+		cout << element << " ";
 }
