a85554d8fa3f1aea182945b44eec4d787aefd79b
안동민 <andongmin94@gmail.com>
2023-10-13 21:42:21 +0900

    .

--- WEB/django/Exercise/Day10/ws_10_2/accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserChangeForm
-from django.contrib.auth import get_user_model
-
-class CustomUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm):
-        model = get_user_model()
-        fields = ('first_name', 'last_name', 'email',)
--- WEB/django/Exercise/Day4/ws_4_2/libraries/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class book(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.TextField()
-    pubdate = models.DateField(auto_now=True)
-    price = models.IntegerField()
-    adult = models.BooleanField()
