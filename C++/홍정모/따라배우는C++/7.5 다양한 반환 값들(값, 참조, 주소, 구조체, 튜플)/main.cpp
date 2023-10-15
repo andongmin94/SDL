6758f5108231658bd1647b51bce0d35d2c4b465d
안동민 <andongmin94@gmail.com>
2023-10-16 02:29:56 +0900

    .

---
+++ C++/홍정모/따라배우는C++/7.5 다양한 반환 값들(값, 참조, 주소, 구조체, 튜플)/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <array>
+#include <tuple>
+
+using namespace std;
+
+tuple<int, double> getTuple()
+{
+	int a = 10;
+	double d = 3.14;
+
+	return make_tuple(a, d);
+}
+
+int main()
+{
+	tuple<int, double> my_tp = getTuple();
+	cout << get<0>(my_tp) << endl;
+	cout << get<1>(my_tp) << endl;
+
+	// C++ 17
+	auto[a, d] = getTuple();
+	cout << a << endl;
+	cout << d << endl;
+}
