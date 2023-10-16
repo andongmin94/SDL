e60db029d0f84f3c7190c258ecef24926887b2cd
안동민 <andongmin94@gmail.com>
2023-10-17 00:26:56 +0900

    .

---
+++ C++/홍정모/따라배우는C++/7.7 함수 오버로딩/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 레퍼런스로 오버로딩하면 만들기 쉬움
+void getRandom(int& x) {};
+void getRandom(double& x) {};
+
+typedef int my_int;
+// 이러면 같은 함수임
+void print(int x) {}
+void print(my_int x) {}
+
+void print(const char* value) {}
+void print(int value) {}
+
+// 모호하면 구분을 못함
+void print(unsigned int value) {}
+void print(float value) {}
+
+int main()
+{
+	int x;
+	getRandom(x);
+	// int x = getRandomInt(x);
+	// int x = getRandom(int());
+
+	print((unsigned int)'a');
+	print(0u);
+	print(3.14159f);
+}
