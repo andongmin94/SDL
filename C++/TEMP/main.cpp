165534eb2eddaf0045cb065d7a008a0c724940fe
안동민 <andongmin94@gmail.com>
2023-10-16 16:12:02 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -1,19 +1,6 @@
 ﻿#include <iostream>
 
 using namespace std;
-
-int min(int a, int b, int c)
-{
-	if (a <= b && a <= c)
-		return a;
-
-	if (b <= a && b <= c)
-		return b;
-
-	if (c <= a && c <= b)
-		return c;
-}
-
 int main()
 {
 	int a, b, c;
