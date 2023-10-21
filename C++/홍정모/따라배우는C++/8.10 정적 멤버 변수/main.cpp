543695ff4f21aaffeb8bda259784ceb90aff19b6
안동민 <andongmin94@gmail.com>
2023-10-21 23:58:27 +0900

    .

---
+++ C++/홍정모/따라배우는C++/8.10 정적 멤버 변수/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Something
+{
+public:
+	static constexpr int s_value = 1;
+};
+
+// int Something::s_value = 1; // define in cpp 중복 선언 문제때문에 헤더 X
+
+int main()
+{
+	cout << &Something::s_value << " " << Something::s_value << endl;
+
+	Something st1;
+	Something st2;
+
+	cout << &st1.s_value << " " << st1.s_value << endl;
+
+	// st1.s_value = 2;
+
+	cout << &st2.s_value << " " << st2.s_value << endl;
+}
---
+++ C++/홍정모/따라배우는C++/8.15 실행 시간 측정하기/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// 그냥 강의 시청
+}
