8b8fbd10dd343589a3f7ca057901c359aac77e04
박규리 <dw05131@naver.com>
2023-10-23 13:15:11 +0900

    .

--- C++/홍정모/따라배우는C++/9.1 산술 연산자 오버로딩 하기/main.cpp
+++ C++/홍정모/따라배우는C++/9.1 산술 연산자 오버로딩 하기/main.cpp
@@ -2,7 +2,24 @@
 
 using namespace std;
 
+class Cents
+{
+private:
+	int m_cents;
+
+public:
+	Cents(int cents) { m_cents = cents; }
+	int getCents() const { return m_cents; }
+	int& getCents() { return m_cents; }
+};
+
+void add(const Cents& c1, const Cents& c2, Cents& c_out)
+{
+	c_out.getCents()
+}
+
 int main()
 {
-
+	Cents cents1(6);
+	Cents cents2(8);
 }
