6a3190fc7f7d432eba585a56efe5abb53a515b7e
안동민 <andongmin94@gmail.com>
2023-10-23 20:27:44 +0900

    .

--- C++/홍정모/따라배우는C++/9.1 산술 연산자 오버로딩 하기/main.cpp
+++ C++/홍정모/따라배우는C++/9.1 산술 연산자 오버로딩 하기/main.cpp
@@ -9,20 +9,19 @@ private:
 
 public:
 	Cents(int cents = 0) { m_cents = cents; }
-
-	// = [] () ->
 	int getCents() const { return m_cents; }
 	int& getCents() { return m_cents; }
 
+	// = [] () ->
 	Cents operator + (const Cents& c2)
 	{
 		return Cents(this->m_cents + c2.m_cents);
 	}
 
-	//friend Cents operator + (const Cents& c1, const Cents& c2)
-	//{
-	//	return Cents(c1.getCents() + c2.getCents());
-	//}
+	friend Cents operator + (const Cents& c1, const Cents& c2)
+	{
+		return Cents(c1.getCents() + c2.getCents());
+	}
 };
 
 //void add(const Cents& c1, const Cents& c2, Cents& c_out)
@@ -37,7 +36,6 @@ int main()
 
 	//Cents sum;
 	//add(cents1, cents2, sum);
-
 	//cout << sum.getCents() << endl;
 
 	//cout << add(cents1, cents2).getCents() << endl;
