35b3e1c81f65d3694c76929c4438a14587ec0ad2
안동민 <andongmin94@gmail.com>
2023-10-23 20:21:12 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-	int a, b, c, d;
-	cin >> a >> b;
-
-	c = (a + b) / 2;
-	d = (a - b) / 2;
-	if (a < b)
-		cout << -1 << endl;
+	long long a;
+	cin >> a;
+	a %= 8;
+	
+	if (a == 1)
+		cout << 1;
+	else if (a == 0 || a == 2)
+		cout << 2;
+	else if (a == 3 || a == 7)
+		cout << 3;
+	else if (a == 4 || a == 6)
+		cout << 4;
 	else
-	{
-		if (c + d == a && c - d == b)
-			cout << c << " " << d;
-		else
-			cout << "-1";
-	}
+		cout << 5;
 }
