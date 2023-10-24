b226a882b47588d20e1d0893dfd7ecf75c0b01b2
안동민 <andongmin94@gmail.com>
2023-10-24 17:49:27 +0900

    .

---
+++ C++/홍정모/따라배우는C++/9.10 변환 생성자, explicit, delete/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+class Fraction
+{
+private:
+	int m_numerator;
+	int m_denominator;
+
+public:
+	// delete라는 형태로 막는거임(explicit)
+	Fraction(char) = delete;
+
+	explicit Fraction(int num = 0, int den = 1)
+		: m_numerator(num), m_denominator(den)
+	{
+		assert(den != 0);
+	}
+
+	Fraction(const Fraction& fraction) // copy constructor
+		: m_numerator(fraction.m_numerator), m_denominator(fraction.m_denominator)
+	{
+		cout << "Copy constructor callded" << endl;
+	}
+
+	friend ostream& operator << (ostream& out, const Fraction& f)
+	{
+		out << f.m_numerator << " / " << f.m_denominator << endl;
+		return out;
+	}
+};
+
+void doSomething(Fraction frac)
+{
+	cout << frac << endl;
+}
+
+int main()
+{
+	Fraction frac('c');
+	Fraction frac(7);
+	doSomething(frac);
+	doSomething(Fraction(7));
+
+	// explicit으로 막힘
+	doSomething(7);
+}
