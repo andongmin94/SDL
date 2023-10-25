5297f2ec6ca9299d43e681a788cc279985a57a4b
안동민 <andongmin94@gmail.com>
2023-10-25 09:58:58 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -1,9 +1,28 @@
 ﻿#include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main()
 {
-	
+	int a, b;
+	cin >> a;
+	if (a == 1010)
+	{
+		a = 10;
+		b = 10;
+	}
+	else
+	{
+		if (a >= 100 && (a % 100) / 10 == 1)
+		{
+			a / 100;
+			b = 10;
+		}
+		else
+		{
+			b = a % 10;
+			a = a / 10;
+		}
+	}
+	cout << a + b << endl;
 }
