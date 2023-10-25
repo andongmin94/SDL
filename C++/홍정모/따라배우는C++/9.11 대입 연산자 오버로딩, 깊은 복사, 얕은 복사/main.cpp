272458bac5cb06199a2c8d5d66d3c82b0cfd4636
안동민 <andongmin94@gmail.com>
2023-10-25 17:50:15 +0900

    .

--- C++/홍정모/따라배우는C++/9.11 대입 연산자 오버로딩, 깊은 복사, 얕은 복사/main.cpp
+++ C++/홍정모/따라배우는C++/9.11 대입 연산자 오버로딩, 깊은 복사, 얕은 복사/main.cpp
@@ -15,14 +15,17 @@ public:
 	{
 		assert(source);
 
-		m_length = strlen(source) + 1;
+		m_length = strlen(source);
 		m_data = new char[m_length];
 
 		for (int i = 0; i < m_length; i++)
 			m_data[i] = source[i];
 
-		m_data[m_length - 1] = '\0';
+		m_data[m_length] = '\0';
 	}
+
+	// 이렇게도 가능하지만 깔끔하게 구현을 권장
+	// MyString(const MyString& source) = delete;
 
 	MyString(const MyString& source)
 	{
@@ -80,14 +83,19 @@ int main()
 {
 	MyString hello("Hello");
 
-	cout << (int*)hello.m_data << endl;
-	cout << hello.getString() << endl;
+	MyString str1 = hello; // MyString str1 = hello;
 
-	{
-		MyString copy = hello;
-		cout << (int*)copy.m_data << endl;
-		cout << copy.getString() << endl;
-	}
+	MyString str2;
+	str2 = hello;
 
-	cout << hello.getString() << endl;
+	//cout << (int*)hello.m_data << endl;
+	//cout << hello.getString() << endl;
+
+	//{
+	//	MyString copy = hello;
+	//	cout << (int*)copy.m_data << endl;
+	//	cout << copy.getString() << endl;
+	//}
+
+	//cout << hello.getString() << endl;
 }
