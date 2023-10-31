9500d6aec4c0b99d98044c3ba4f4c95598ba8f57
andongmin94 <andongmin94@gmail.com>
2023-10-31 14:10:41 +0900

    .

--- WEB/Database/03-01-many-to-many-relationships/articles/urls.py
+++ WEB/Database/03-01-many-to-many-relationships/articles/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
         views.comments_delete,
         name='comments_delete',
     ),
-    path('<int:article_pk>/likes/', views.likes, name='likes'),
 ]
--- WEB/Django/10-02-django-rest-framework/articles/serializers.py
+++ WEB/Django/10-02-django-rest-framework/articles/serializers.py
@@ -1,18 +1,5 @@
 from rest_framework import serializers
-from .models import Article, Comment
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class ArticleSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Article
-            fields = ('title',)
-
-    article = ArticleSerializer(read_only=True)
-
-    class Meta:
-        model = Comment
-        fields = '__all__'
+from .models import Article
 
 
 class ArticleListSerializer(serializers.ModelSerializer):
@@ -22,10 +9,6 @@ class ArticleListSerializer(serializers.ModelSerializer):
 
 
 class ArticleSerializer(serializers.ModelSerializer):
-    comment_set = CommentSerializer(many=True, read_only=True)
-    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
-
-
     class Meta:
         model = Article
         fields = '__all__'
