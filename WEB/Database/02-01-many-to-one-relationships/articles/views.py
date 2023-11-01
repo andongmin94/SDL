f7a96adbcb7a65de91dbc6ec09b7f90afdb2cd55
안동민 <andongmin94@gmail.com>
2023-11-01 22:22:11 +0900

    .

--- WEB/Database/02-01-many-to-one-relationships/articles/views.py
+++ WEB/Database/02-01-many-to-one-relationships/articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Article, Comment
-from .forms import ArticleForm, CommentForm
+from .models import Article
+from .forms import ArticleForm
 
 
 # Create your views here.
@@ -15,12 +15,8 @@ def index(request):
 
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
-    comment_form = CommentForm()
-    comments = article.comment_set.all()
     context = {
         'article': article,
-        'comment_form' : comment_form,
-        'comments' : comments,
     }
     return render(request, 'articles/detail.html', context)
 
@@ -62,24 +58,3 @@ def update(request, pk):
         'form': form,
     }
     return render(request, 'articles/update.html', context)
-
-
-def comments_create(request, pk):
-    article = Article.objects.get(pk=pk)
-    comment_form = CommentForm(request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.article = article
-        comment_form.save()
-        return redirect('articles:detail', article.pk)
-    context = {
-        'article' : article,
-        'comment_form' : comment_form,
-    }
-    return render(request, 'articles/detail.html', context)
-
-
-def comments_delete(request, article_pk, comment_pk):
-    comment = Comment.objects.get(pk=comment_pk)
-    comment.delete()
-    return redirect('articles:detail', article_pk)
