8acd0f373cddf4bb42bbe7ae1019e4b9cb314815
andongmin94 <andongmin94@gmail.com>
2023-11-02 17:53:30 +0900

    .

--- C++/홍정모/따라배우는C++/11.4 유도된 클래스들의 생성과 초기화/main.cpp
+++ C++/홍정모/따라배우는C++/11.4 유도된 클래스들의 생성과 초기화/main.cpp
@@ -2,7 +2,51 @@
 
 using namespace std;
 
+class A
+{
+public:
+	A(int a)
+	{
+		cout << "A : " << a << endl;
+	}
+
+	~A()
+	{
+		cout << "소멸자 A" << endl;
+	}
+};
+
+class B : public A
+{
+public:
+	B(int a, double b)
+		: A(a)
+	{
+		cout << "B : " << b << endl;
+	}
+
+	~B()
+	{
+		cout << "소멸자 B" << endl;
+	}
+};
+
+class C : public B
+{
+public:
+	C(int a, double b, char c)
+		: B(a, b)
+	{
+		cout << "C : " << c << endl;
+	}
+
+	~C()
+	{
+		cout << "소멸자 C" << endl;
+	}
+};
+
 int main()
 {
-
+	C c(1024, 3.14, 'a');
 }
