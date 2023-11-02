b43debfde1b407b938a8df08bb1581c0678c7dfd
andongmin94 <andongmin94@gmail.com>
2023-11-02 17:53:30 +0900

    .

---
+++ C++/홍정모/따라배우는C++/11.5 상속과 접근 지정자/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Base
+{
+public:
+	int m_public;
+
+protected:
+	int m_protected;
+
+private:
+	int m_private;
+};
+
+class Derived : private Base
+{
+public:
+	Derived()
+	{
+		m_public = 1024;
+		m_protected = 1024;
+	}
+};
+
+class GrandChild : public Derived
+{
+public:
+	GrandChild()
+	{
+		m_public;
+	}
+};
+
+int main()
+{
+	Derived derived;
+	derived.m_public = 1024;
+}
