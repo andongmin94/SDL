e5c47955ee56c8b9aa4289db9d52f81d80251a75
andongmin94 <andongmin94@gmail.com>
2023-11-03 17:48:18 +0900

    .

--- C++/홍정모/따라배우는C++/11.7 상속받은 함수를 오버라이딩 하기/main.cpp
+++ C++/홍정모/따라배우는C++/11.7 상속받은 함수를 오버라이딩 하기/main.cpp
@@ -2,7 +2,70 @@
 
 using namespace std;
 
+class Base
+{
+protected:
+	int m_i;
+
+public:
+	Base(int value)
+		: m_i(value)
+	{}
+
+	void print()
+	{
+		cout << "I'm Base" << endl;
+	}
+
+	friend std::ostream& operator << (std::ostream& out, const Base& b)
+	{
+		cout << "This is base output" << endl;
+		return out;
+	}
+};
+
+class Derived : public Base
+{
+private:
+	double m_d;
+
+public:
+	Derived(int value)
+		: Base(value)
+	{}
+
+	void print()
+	{
+		Base::print();
+		cout << "I'm Derived" << endl;
+	}
+
+	friend std::ostream& operator << (std::ostream& out, const Derived& b)
+	{
+		cout << static_cast<Base>(b);
+		cout << "This is derived output" << endl;
+		return out;
+	}
+};
+
 int main()
 {
+	Base base(10);
+	base.print();
+	cout << endl;
 
+	Derived derived(20);
+	derived.Base::print();
+	cout << endl;
+
+	derived.print();
+	cout << endl;
+
+	cout << base;
+	cout << endl;
+
+	cout << static_cast<Base>(derived);
+	cout << endl;
+
+	cout << derived;
 }
---
+++ C++/홍정모/따라배우는C++/12.3 override, final, 공변 반환값/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	virtual void print(int x) { cout << "A" << endl; }
+};
+
+class B : public A
+{
+public:
+	void print(short x) override { cout << "B" << endl; }
+};
+
+//class C : public B
+//{
+//public:
+//	virtual void print() { cout << "C" << endl; }
+//};
+
+int main()
+{
+	A a;
+	B b;
+	//C c;
+
+	A& ref = b;
+	ref.print(1);
+}
