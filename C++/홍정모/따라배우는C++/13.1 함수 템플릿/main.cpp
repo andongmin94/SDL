e1e9cc5e217c3099593acfc57117f2e3bc3f58f9
안동민 <andongmin94@gmail.com>
2023-11-04 23:25:46 +0900

    .

--- C++/홍정모/따라배우는C++/13.1 함수 템플릿/main.cpp
+++ C++/홍정모/따라배우는C++/13.1 함수 템플릿/main.cpp
@@ -1,4 +1,20 @@
 ﻿#include <iostream>
+//#include "Cents.h"
+
+int getMax(int x, int y)
+{
+	return (x > y) ? x : y;
+}
+
+double getMax(double x, double y)
+{
+	return (x > y) ? x : y;
+}
+
+float getMax(float x, float y)
+{
+	return (x > y) ? x : y;
+}
 
 using namespace std;
 
