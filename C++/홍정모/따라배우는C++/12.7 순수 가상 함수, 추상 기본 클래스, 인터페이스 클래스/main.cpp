0e1c464646b4d8dc886001011bafa52ce71f72cc
안동민 <andongmin94@gmail.com>
2023-11-04 23:23:48 +0900

    .

--- C++/홍정모/따라배우는C++/12.7 순수 가상 함수, 추상 기본 클래스, 인터페이스 클래스/main.cpp
+++ C++/홍정모/따라배우는C++/12.7 순수 가상 함수, 추상 기본 클래스, 인터페이스 클래스/main.cpp
@@ -5,10 +5,75 @@ using namespace std;
 
 class Animal
 {
-	protected
+protected:
+	string m_name;
+
+public:
+	Animal(std::string name)
+		: m_name(name)
+	{}
+
+public:
+	string getName() { return m_name; }
+
+	virtual void speak() const = 0; // pure virtual function
+
+	//virtual void sepak() const
+	//{
+	//	cout << m_name << " ??? " << endl;
+	//}
 };
+
+//void Animal::speak() const // the body of the pure virtual function
+//{
+//	cout << m_name << " ??? " << endl;
+//}
+
+class Cat : public Animal
+{
+public:
+	Cat(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Meow " << endl;
+	}
+};
+
+class Dog : public Animal
+{
+public:
+	Dog(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Woof " << endl;
+	}
+};
+
+class Cow : public Animal
+{
+public:
+	Cow(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Mooo " << endl;
+	}
+};
+
 
 int main()
 {
+	//Animal ani("Hi");
+	//ani.speak();
 
+	Cow cow("hello");
+	cow.speak();
 }
