3019f14cca010699f9c50dd076d38b2b88747c81
안동민 <andongmin94@gmail.com>
2023-11-06 17:57:30 +0900

    .

--- C++/홍정모/따라배우는C++/13.1 함수 템플릿/main.cpp
+++ C++/홍정모/따라배우는C++/13.1 함수 템플릿/main.cpp
@@ -1,24 +1,18 @@
-﻿#include <iostream>
-//#include "Cents.h"
-
-int getMax(int x, int y)
-{
-	return (x > y) ? x : y;
-}
-
-double getMax(double x, double y)
-{
-	return (x > y) ? x : y;
-}
-
-float getMax(float x, float y)
-{
-	return (x > y) ? x : y;
-}
+﻿#include "Cents.h"
 
 using namespace std;
 
+template<typename T>
+T getMax(T x, T y)
+{
+	return (x > y) ? x : y;
+}
+
 int main()
 {
+	cout << getMax(1, 2) << endl;
+	cout << getMax(3.14, 15.3) << endl;
+	cout << getMax('a', 'c') << endl;
 
+	cout << getMax(Cents(5), Cents(9)) << endl;
 }
---
+++ C++/홍정모/따라배우는C++/13.4 함수 템플릿 특수화/Storage.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <iostream>
+
+template<typename T>
+class Storage
+{
+private:
+	T m_value;
+
+public:
+	Storage(T value)
+	{
+		m_value = value;
+	}
+
+	~Storage()
+	{}
+
+	void print()
+	{
+		std::cout << m_value << std::endl;
+	}
+};
+
+template<>
+void Storage<double>::print()
+{
+	std::cout << "Double Type" << std::endl;
+	std::cout << std::scientific << m_value << std::endl;
+}
