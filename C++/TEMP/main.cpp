d66a8debed635387b231e8ef8852637bbdb61a7a
안동민 <andongmin94@gmail.com>
2023-11-07 17:51:41 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -5,15 +5,27 @@ using namespace std;
 
 long long recur(long long a)
 {
-	int b = 0;
-	int c = 0;
-
-
-	return
+	if (a >= 0 || a < 9)
+		return -1;
+	long long b = a;
+	int c = 1;
+	while (a > 9)
+	{
+		a /= 10;
+		c++;
+	}
+	if (b == (a * c))
+		return 0;
+	return recur(a * c);
 }
 
 int main()
 {
 	long long a;
-	cin >> a
+	cin >> a;
+	if (recur(a) == -1)
+		cout << "NFA";
+	else
+		cout << "FA";
+
 }
--- C++/홍정모/따라배우는C++/13.5 클래스 템플릿 특수화/main.cpp
+++ C++/홍정모/따라배우는C++/13.5 클래스 템플릿 특수화/main.cpp
@@ -8,16 +8,66 @@ template<typename T>
 class A
 {
 public:
+	A(const T& input)
+	{}
+
 	void doSomething()
 	{
-		cout << typeid(T).name << endl;
+		cout << typeid(T).name() << endl;
 	}
 
 	void test()
 	{}
 };
 
+template<>
+class A<char>
+{
+public:
+	A(const char& temp)
+	{}
+
+	void doSomething()
+	{
+		cout << "Char type secialization" << endl;
+	}
+};
+
 int main()
 {
+	
+	A<int> a_int(1);
+	A<double> a_double(3.14);
+	A<char> a_char('a');
+
+	a_int.test();
+	a_double.test();
+	// a_char.test(); 사실상 다른 클래스를 구현하는 셈
+
+	a_int.doSomething();
+	a_double.doSomething();
+	a_char.doSomething();
+
+	// Define a Storage8 for integer
+	Storage8<int> intStorage;
+
+	for (int count = 0; count < 8; count++)
+		intStorage.set(count, count);
+
+	for (int count = 0; count < 8; count++)
+		cout << intStorage.get(count) << endl;
+
+	cout << "Sizeof Storage8<int> " << sizeof(Storage8<int>) << endl;
+
+	// Define a Storage8 for bool
+	Storage8<bool> boolStorage;
+
+	for (int count = 0; count < 8; count++)
+		boolStorage.set(count, count & 3);
+
+	for (int count = 0; count < 8; count++)
+		cout << (boolStorage.get(count) ? "true" : "false") << endl;
+
+	cout << "Sizeof Storage8<bool> " << sizeof(Storage8<bool>) << endl;
 
 }
