9c11111b53c5f35d322282534dafae14bd5e9d6d
안동민 <andongmin94@gmail.com>
2023-11-07 17:51:41 +0900

    .

---
+++ C++/홍정모/따라배우는C++/13.8 멤버 함수를 한 번 더 템플릿화 하기/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<class T>
+class A
+{
+private:
+	T m_value;
+
+public:
+	A(const T& input)
+		: m_value(input)
+	{}
+
+	template<typename TT>
+	void doSomething(const TT& input)
+	{
+		cout << typeid(T).name() << " " << typeid(TT).name() << endl;
+		cout << (TT)m_value << endl;
+	}
+
+	void print()
+	{
+		cout << m_value << endl;
+	}
+};
+
+int main()
+{
+	A<int> a_int(123);
+	a_int.print();
+
+	a_int.doSomething<float>(123.4);
+	// 파라미터로 구분이 가능하면 제네릭을 생략할 수 있음.
+	// a_int.doSomething(123.4);
+
+	cout << endl;
+
+	A<char> a_char('A');
+	a_char.doSomething(1);
+	a_char.doSomething<int>(1);
+	a_char.doSomething(1);
+	a_char.doSomething(int());
+	// 정의에 파라미터가 없어도 스페셜라이제이션이 가능함.
+	// a_char.doSomething<int>();
+}
---
+++ C++/홍정모/따라배우는C++/14.1 예외처리의 기본/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int findFirstChar(const char* string, char ch)
+{
+	for (std::size_t index = 0; index < strlen(string); index++)
+		if (string[index] == ch)
+			return index;
+
+	return -1; // no match
+}
+
+double divide(int x, int y, bool& success)
+{
+	if (y == 0)
+	{
+		success = false;
+		return 0.0;
+	}
+
+	success = false;
+	return static_cast<double>(x) / y;
+}
+
+int main()
+{
+
+}
