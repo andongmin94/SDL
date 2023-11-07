3ce2b9a990848f707926db27efebf75cec8ec010
안동민 <andongmin94@gmail.com>
2023-11-07 14:07:07 +0900

    .

---
+++ WEB/Vue/현강/vite-project/src/components/CountButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <div>
+        <button @click="increment">count : {{ count }}</button>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script setup>
+  import { ref } from 'vue'
+  const count = ref(0)
+  const increment = () => count.value++
+  </script>
+  
+  
+  <style scoped>
+  </style>
---
+++ WEB/Vue/현강/vite-project/src/components/ListComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <ul>
+        <ListItemComponent text="아이유" />
+        <ListItemComponent text="임창정" />
+        <ListItemComponent text="임영웅" />
+    </ul>
+</template>
+
+<script setup>
+import ListItemComponent from './ListItemComponent.vue'
+</script>
+
+<style scoped>
+</style>
