ffdd48a776c5122f0b8c12f8243f2d6e59546d4a
안동민 <andongmin94@gmail.com>
2023-11-09 18:01:55 +0900

    .

---
+++ C++/홍정모/따라배우는C++/15.1 이동의 의미와 스마트 포인터/AutoPtr.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <iostream>
+
+// 쓰지 않음. std::auto_ptr<int>; // c++ 98, c++ 11, c++ 17;
+
+template<class T>
+class AutoPtr
+{
+public:
+	T* m_ptr = nullptr;
+
+public:
+	AutoPtr(T* ptr = nullptr)
+		: m_ptr(ptr)
+	{}
+
+	~AutoPtr()
+	{
+		if (m_ptr != nullptr) delete m_ptr;
+	}
+
+	AutoPtr(AutoPtr& a)
+	{
+		m_ptr = a.m_ptr;
+		a.m_ptr = nullptr;
+	}
+
+	AutoPtr& operator = (AutoPtr& a)
+	{
+		if (&a == this)
+			return *this;
+	}
+};
---
+++ C++/홍정모/따라배우는C++/15.1 이동의 의미와 스마트 포인터/main.cpp
@@ -0,0 +1,32 @@
+﻿#include "Resource.h"
+#include "AutoPtr.h"
+
+using namespace std;
+
+// RAII : resource acquisition is initialization
+
+void doSomething()
+{
+	try
+	{
+		Resource* res = new Resource;
+
+		// work with res
+		if (false)
+		{
+			throw - 1; // exception
+		}
+
+		delete res;
+	}
+	catch (...)
+	{
+
+	}
+	return;
+}
+
+int main()
+{
+	doSomething();
+}
