276c37188140d444de2b1dba00f0f891d21f085b
안동민 <andongmin94@gmail.com>
2023-11-10 09:03:39 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -1,16 +1,21 @@
-﻿#include <iostream>
+﻿#include<iostream>
 
 using namespace std;
 
-int main()
-{
-	int a, b[5], c = 0, d = 0;
-	cin >> a;
-	for (int i = 0; i < 5; i++)
-	{
-		cin >> b[i];
-		if (a == b[i])
-			c++;
-	}
-	cout << c;
+int main() {
+    int a, b, c, d, e, f, g, h, i;
+    cin >> a >> b >> c >> d >> e >> f;
+
+    if (b < e)
+        g = d - a;
+    else if (b == e && c <= f)
+        g = d - a;
+    else
+        g = d - a - 1;
+
+    h = d - a + 1;
+
+    i = d - a;
+
+    cout << g << endl << h << endl << i;
 }
