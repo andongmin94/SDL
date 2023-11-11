470b0b0d52af6e1c28fdf5e1a9f5f86347ef1e6e
안동민 <andongmin94@gmail.com>
2023-11-11 20:34:32 +0900

    Update main.cpp

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -3,19 +3,18 @@
 using namespace std;
 
 int main() {
-    int a, b, c, d, e, f, g, h, i;
-    cin >> a >> b >> c >> d >> e >> f;
 
-    if (b < e)
-        g = d - a;
-    else if (b == e && c <= f)
-        g = d - a;
-    else
-        g = d - a - 1;
+    unsigned long long a = -1;
 
-    h = d - a + 1;
+    while (1)
+    {
+        cin >> a;
+        if (a == 0)
+            break;
 
-    i = d - a;
-
-    cout << g << endl << h << endl << i;
+        if (a % 42 == 0)
+            cout << "PREMIADO" << endl;
+        else
+            cout << "TENTE NOVAMENTE" << endl;
+    }
 }
