7c731dfa256104e16844c146d34a430c583a76b4
안동민 <andongmin94@gmail.com>
2023-11-14 08:25:57 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -6,10 +6,5 @@ int main() {
 
     int a, b;
     cin >> a >> b;
-    if (a >= 3 && b <= 4)
-        cout << "TroyMartian" << endl;
-    if (a <= 6 && b >= 2)
-        cout << "VladSaturnian" << endl;
-    if (a <= 2 && b <= 3)
-        cout << "GraemeMercurian" << endl;
+    cout << min(a, b) / 2 << endl;
 }
