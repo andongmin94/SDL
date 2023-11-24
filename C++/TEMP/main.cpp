19ca5ee2568373d3d66e8ea6a149384f90b36d01
안동민 <andongmin94@gmail.com>
2023-11-24 11:10:53 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main() {
 
+<<<<<<< Updated upstream
     int a = 91;
     int b, c, d;
     cin >> b >> c >> d;
     c = c * 3;
     cout << "The 1-3-sum is " << a + b + c + d;
+=======
+    int a, b;
+    cin >> a >> b;
+    cout << min(a / 2, b);
+>>>>>>> Stashed changes
 }
