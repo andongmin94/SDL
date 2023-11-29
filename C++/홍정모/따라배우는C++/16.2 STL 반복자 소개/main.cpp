0713a89704b0e516d60fb994030e758fd66c897a
andongmin <andongmin94@gmail.com>
2023-11-29 23:21:50 +0900

    .

--- C++/홍정모/따라배우는C++/16.2 STL 반복자 소개/main.cpp
+++ C++/홍정모/따라배우는C++/16.2 STL 반복자 소개/main.cpp
@@ -1,8 +1,19 @@
 ﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
 
 using namespace std;
 
 int main()
 {
+	map<int, char> container;
+	for (int i = 0; i < 10; i++)
+		container.insert(make_pair(i, char(i+65)));
 
+	for (auto itr = container.begin(); itr != container.end(); itr++)
+		//cout << *itr << " ";
+		cout << itr->first << " " << itr->second << endl;
+	cout << endl;
 }
--- C++/홍정모/따라배우는C++/16.3 STL 알고리즘 소개/main.cpp
+++ C++/홍정모/따라배우는C++/16.3 STL 알고리즘 소개/main.cpp
@@ -1,8 +1,42 @@
 ﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
 
 using namespace std;
 
 int main()
 {
+	list<int> container;
+	for (int i = 0; i < 10; i++)
+		container.push_back(i);
 
+	//auto itr = li.begin();
+	//list<int>::const_iterator itr;
+	auto itr = min_element(container.begin(), container.end());
+	cout << *itr << endl;
+
+	itr = max_element(container.begin(), container.end());
+	cout << *itr << endl;
+
+	cout << endl;
+
+	itr = find(container.begin(), container.end(), 3);
+	container.insert(itr, 128);
+
+	for (auto& e : container) cout << e << " ";
+	cout << endl;
+
+	//sort(container.begin(), container.end());
+	container.sort();
+
+	for (auto& e : container) cout << e << " ";
+	cout << endl;
+
+	//reverse(container.begin(), container.end());
+	container.reverse();
+
+	for (auto& e : container) cout << e << " ";
+	cout << endl;
 }
