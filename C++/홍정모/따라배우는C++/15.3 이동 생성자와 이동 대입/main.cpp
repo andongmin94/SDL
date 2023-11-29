de6505976c514b5e713ec2ddae29e22a14422be1
andongmin <andongmin94@gmail.com>
2023-11-29 17:43:07 +0900

    .

--- C++/홍정모/따라배우는C++/15.3 이동 생성자와 이동 대입/main.cpp
+++ C++/홍정모/따라배우는C++/15.3 이동 생성자와 이동 대입/main.cpp
@@ -1,8 +1,28 @@
-﻿#include <iostream>
+﻿#include "Timer.h"
+#include "AutoPtr.h"
+#include "Resource.h"
+
+AutoPtr<Resource> generateResource()
+{
+    AutoPtr<Resource> res(new Resource(10000000));
+
+    return res;
+}
 
 using namespace std;
 
 int main()
 {
-    cout << 1 << endl;
+    streambuf* orig_buf = cout.rdbuf();
+    //cout.rdbuf(NULL); // discounnect cout from buffer
+
+    //Timer timer;
+
+    {
+        AutoPtr<Resource> main_res;
+        main_res = generateResource();
+    }
+
+    cout.rdbuf(orig_buf);
+    //cout << Timer.elapsed() << endl;
 }
--- C++/홍정모/따라배우는C++/15.5 unique_ptr/main.cpp
+++ C++/홍정모/따라배우는C++/15.5 unique_ptr/main.cpp
@@ -1,8 +1,84 @@
 ﻿#include <iostream>
+#include <memory>
+#include "Resource.h"
 
 using namespace std;
 
+auto doSomething()
+{
+	//auto res1 = std::make_unique<Resource>(5);
+	//return res1;
+
+	return std::unique_ptr<Resource>(new Resource(5));
+
+	//return std::make_unique<Resource>(5);
+}
+
+//void doSomething2(std::unique_ptr<Resource>&res)
+void doSomething2(Resource* res)
+{
+	res->setAll(10);
+	res->print();
+}
+
 int main()
 {
+	//{
+	//	//Resource* res = new Resource(10000000);
 
+	//	//std::unique_ptr<Resource> res(new Resource(10000000));
+
+	//	//early return or throw
+
+	//	//delete res;
+	//}
+
+	//{
+	//	std::unique_ptr<int> upi(new int);
+
+	//	//std::unique_ptr<Resource> res1(new Resource(5));
+	//	auto res1 = std::make_unique<Resource>(5);
+	//	//auto res1 = doSomething();
+
+	//	res1->setAll(5);
+	//	res1->print();
+
+	//	std::unique_ptr<Resource> res2;
+
+	//	std::cout << std::boolalpha;
+	//	std::cout << static_cast<bool>(res1) << std::endl;
+	//	std::cout << static_cast<bool>(res2) << std::endl;
+
+	//	//res2 = res1;
+	//	res2 = std::move(res1);
+
+	//	std::cout << std::boolalpha;
+	//	std::cout << static_cast<bool>(res1) << std::endl;
+	//	std::cout << static_cast<bool>(res2) << std::endl;
+
+	//	if (res1 != nullptr) res1->print();
+	//	if (res2 != nullptr) res2->print(); // (*res2).print();
+	//}
+
+	//{
+	//	auto res1 = std::make_unique<Resource>(5);
+	//	res1->setAll(1);
+	//	res1->print();
+
+	//	//doSomething2(res1);
+	//	doSomething2(res1.get());
+
+	//	res1->print();
+	//}
+
+	{
+		Resource* res = new Resource;
+		std::unique_ptr<Resource> res1(res);
+		std::unique_ptr<Resource> res2(res);
+
+		delete res;
+	}
+	// 이렇게 사용하면 문제가 발생함.
+	// 유니크인데 두 번 줌.
+	// 유니크가 메모리 해제하는데 메모리 해제를 마지막에 또 함.
 }
