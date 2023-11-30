d43cb5c290145a2866250a6d67831216a63a316c
andongmin <andongmin94@gmail.com>
2023-12-01 03:25:27 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -4,33 +4,9 @@ using namespace std;
 
 int main()
 {
-    int a, b;
-
-    int rising = 0;
-    int down = 0;
-    int same = 0;
-    cin >> b;
-
-    a = b;
-
-    for (int i = 0; i < 3; i++)
-    {
-        cin >> b;
-        if (b > a)
-            rising++;
-        if (b < a)
-            down++;
-        if (b == a)
-            same++;
-        a = b;
-    }
-    if (rising == 3)
-        cout << "Fish Rising" << endl;
-    else if (down == 3)
-        cout << "Fish Diving" << endl;
-    else if (same == 3)
-        cout << "Fish At Constant Depth" << endl;
-    else
-        cout << "No Fish" << endl;
-
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a + b * 2 + c * 3 >= 10)
+        cout << "happy" << endl;
+    else cout << "sad" << endl;
 }
--- C++/홍정모/따라배우는C++/17.2 string의 여러가지 생성자들과 형변환/main.cpp
+++ C++/홍정모/따라배우는C++/17.2 string의 여러가지 생성자들과 형변환/main.cpp
@@ -1,8 +1,39 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
+template<typename T>
+std::string ToString(T x)
+{
+	std::ostringstream osstream;
+
+	osstream << x;
+	return osstream.str();
+}
+
+template<typename T>
+bool FromString(const std::string& str, T& x)
+{
+	std::istringstream isstream(str);
+
+	return (isstream >> x) ? true : false;
+}
+
 int main()
 {
+	std::string my_str(ToString("Hello"));
 
+	std::cout << my_str << std::endl;
+
+	//float i = std::stof(my_str);
+	//cout << i << endl;
+
+	double d;
+
+	if (FromString(my_str, d))
+		cout << d << endl;
+	else cout << "Cannot convert string to double" << endl;
 }
