f185c1bf444f850e05c249d93a8e55b6034b8f66
andongmin <andongmin94@gmail.com>
2023-12-04 20:10:30 +0900

    .

--- C++/홍정모/따라배우는C++/18.5 정규 표현식 소개/main.cpp
+++ C++/홍정모/따라배우는C++/18.5 정규 표현식 소개/main.cpp
@@ -1,8 +1,27 @@
 ﻿#include <iostream>
+#include <regex>
 
 using namespace std;
 
 int main()
 {
+	regex re("\\d");
+	regex re("[[:digit:]]{3}");
+	regex re("[A-Z]+");
+	regex re("[A-Z]{3}");
+	regex re("([0-9]{1})([-]?)([0-9]{1,4})");
 
+	string str;
+
+	while (true)
+	{
+		getline(cin, str);
+
+		if (std::regex_match(str, re))
+			cout << "Match" << endl;
+		else
+			cout << "No match" << endl;
+
+		// print matches
+	}
 }
