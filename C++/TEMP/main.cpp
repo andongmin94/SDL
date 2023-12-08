ceeb2d07e15fef6b05a3ae0a0349aedf81f54bc0
andongmin <andongmin94@gmail.com>
2023-12-09 06:53:45 +0900

    Update main.cpp

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -16,9 +16,5 @@ int main()
     k.push_back(c);
     sort(k.begin(), k.end());
 
-    if (a == b && b == c)
-         cout << 2 << endl;
-    else if (k[0] * k[0] + k[1] * k[1] == k[2] * k[2])
-         cout << 1 << endl;
-    else cout << 0 << endl;
+    cout << k[2] + k[1] << endl;
 }
