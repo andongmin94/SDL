5e0a7ef7d76ecaf87b970797b4abf26052ebc2d9
andongmin <andongmin94@gmail.com>
2023-12-08 19:15:28 +0900

    .

--- C++/홍정모/따라배우는C++/19.2 C++ 17 함수에서 여러 개의 리턴값 반환하기/main.cpp
+++ C++/홍정모/따라배우는C++/19.2 C++ 17 함수에서 여러 개의 리턴값 반환하기/main.cpp
@@ -1,8 +1,20 @@
 ﻿#include <iostream>
+#include <tuple>
 
 using namespace std;
 
+auto my_func()
+{
+	return tuple(123, 456, 789, 10);
+}
+
 int main()
 {
+	//auto result = my_func();
 
+	//cout << get<0>(result) << " " << get<1>(result) << " " << get<2>(result) << endl;
+
+	auto [a, b, c, d] = my_func();
+
+	cout << a << " " << b << " " << c << " " << d << endl;
 }
