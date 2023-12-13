3bb895f0bcf2aed8a7541d1e6693e86148484953
andongmin <andongmin94@gmail.com>
2023-12-13 17:17:28 +0900

    .

--- C++/홍정모/따라배우는C++/19.7 완벽한 전달과 forward/main.cpp
+++ C++/홍정모/따라배우는C++/19.7 완벽한 전달과 forward/main.cpp
@@ -1,6 +1,21 @@
 ﻿#include <iostream>
+#include <vector>
+#include <utility> // std::forward
 
 using namespace std;
+
+struct MyStruct
+{};
+
+void func(MyStruct& s)
+{
+	cout << "Pass by L-ref" << endl;
+}
+
+void func(MyStruct&& s)
+{
+	cout << "Pass by R-ref" << endl;
+}
 
 int main()
 {
