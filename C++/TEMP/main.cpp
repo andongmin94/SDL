a67825f3c2558b85b49e2da667de44036025611b
andongmin <andongmin94@gmail.com>
2023-12-13 17:17:28 +0900

    .

--- C++/TEMP/main.cpp
+++ C++/TEMP/main.cpp
@@ -1,25 +1,13 @@
 ﻿#include <iostream>
+#include <iomanip>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
-	bool check = true;
-
-	if (a == 8 || a == 9)
-		check = false;
-
-	if (check == false && b == c)
-		check = false;
-	else check = true;
-
-	if (check == false && d == 8 || d == 9)
-		check = false;
-	else check = true;
-
-	if (check == true)
-		cout << "answer";
-	else cout << "ignore";
+	long double a;
+	cin >> a;
+	cout << fixed << setprecision(9);
+	cout << sqrt(a) * 4 << '\n';
 }
