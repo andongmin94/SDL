285f0a60e294f4c5df9ff29f0958d25995a28b38
andongmin <andongmin94@gmail.com>
2023-12-16 14:01:42 +0900

    .

--- C++/Solution/Solution.cpp
+++ C++/Solution/Solution.cpp
@@ -4,17 +4,14 @@ using namespace std;
 
 int main()
 {
-	int a;
-	cin >> a;
-	cout << "Gnomes:" << endl;
-	for (int i = 0; i < a; i++)
+	int a, b, c;
+	int cnt = 0;
+	cin >> a >> b >> c;
+
+	while (a < b)
 	{
-		int b, c, d;
-		cin >> b >> c >> d;
-		if (b < c && c < d)
-			cout << "Ordered" << endl;
-		else if (b > c && c > d)
-			cout << "Ordered" << endl;
-		else cout << "Unordered" << endl;
+		a += c;
+		cnt++;
 	}
+	cout << cnt << endl;
 }
---
+++ C++/홍정모/따라배우는C++/챕터 04/04.03 Using문과 모호성/Main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+namespace a
+{
+	int my_var(10);
+	int my_a(123);
+}
+
+namespace b
+{
+	int my_var(20);
+	int my_b(123);
+}
+
+int main()
+{
+	using namespace std;
+	
+	
+	{
+		using namespace a;
+		cout << my_var << endl;
+	}
+
+	{
+		using namespace b;
+		cout << b::my_var << endl;
+	}
+
+	//cout << my_a << endl;
+	//cout << my_b << endl;
+}
