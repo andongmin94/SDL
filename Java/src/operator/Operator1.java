b75e0433f541686b4aeca3562c5d28e75e7382c5
andongmin <andongmin94@gmail.com>
2023-12-18 16:21:22 +0900

    .

---
+++ Java/src/operator/Operator1.java
@@ -0,0 +1,9 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5;
+        int b = 2;
+    }
+}
--- Java/src/variable/Var8.java
+++ Java/src/variable/Var8.java
@@ -6,5 +6,11 @@ public class Var8 {
         byte b = 127; // -128 ~ 127
         short s = 32767; // -32.768 ~ 32.767
         int i = 2147483647;
+
+        long l = 123L;
+
+        // 실수
+        float f = 10.0f;
+        double d = 10.0;
     }
 }
