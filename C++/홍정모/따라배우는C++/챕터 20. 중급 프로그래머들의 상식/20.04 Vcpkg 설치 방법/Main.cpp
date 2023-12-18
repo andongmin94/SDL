5bcf0f4b213155e7ba4e1f3df6987ffe367dc470
andongmin <andongmin94@gmail.com>
2023-12-18 13:56:54 +0900

    .

--- C++/홍정모/따라배우는C++/챕터 20. 중급 프로그래머들의 상식/20.04 Vcpkg 설치 방법/Main.cpp
+++ C++/홍정모/따라배우는C++/챕터 20. 중급 프로그래머들의 상식/20.04 Vcpkg 설치 방법/Main.cpp
@@ -1,8 +1,13 @@
 ﻿#include <iostream>
+#include <boost/asio.hpp>
 
 using namespace std;
 
 int main()
 {
-
+	// https://github.com/microsoft/vcpkg
+	// git clone https://github.com/microsoft/vcpkg
+	// bootstrap-vcpkg
+	// vcpkg.exe install [package]:x64-windows
+	// vcpkg integrate all의 과정이 필요함.
 }
--- C++/홍정모/따라배우는C++/챕터 20. 중급 프로그래머들의 상식/20.05 TCP IP 네트워킹 맛보기 - Boost, Asio Socket IOStream/Main.cpp
+++ C++/홍정모/따라배우는C++/챕터 20. 중급 프로그래머들의 상식/20.05 TCP IP 네트워킹 맛보기 - Boost, Asio Socket IOStream/Main.cpp
@@ -1,8 +1,90 @@
-﻿#include <iostream>
+﻿// Server
 
-using namespace std;
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
 
 int main()
 {
+	try
+	{
+		boost::asio::io_service io_service;
 
+		tcp::endpoint endpoint(tcp::v4(), 13);
+		tcp::acceptor acceptor(io_service, endpoint);
+
+		std::cout << "Server started" << std::endl;
+
+		for (;;) // while(true)
+		{
+			const std::string message_to_send = "Hello From Server";
+
+			boost::asio::ip::tcp::iostream stream;
+
+			std::cout << "check 1" << std::endl;
+
+			boost::system::error_code ec;
+			acceptor.accept(*stream.rdbuf(), ec);
+
+			std::cout << "check 2" << std::endl;
+
+			if (!ec) // TODO: How to take care of multiple clients? Multi-threading?
+			{
+				// receive message from client
+				std::string line;
+				std::getline(stream, line);
+
+				// send message to client
+				stream << message_to_send;
+				stream << std::endl; // send std::endl to end getline of client
+			}
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+// Client
+
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if (argc != 2)
+		{
+			std::cerr << "Usage: client <host>" << std::endl;
+			return 1;
+		}
+
+		tcp::iostream stream(argv[1], std::to_string(int(13))); // port number 13
+		if (!stream)
+		{
+			std::cout << "Unable to connect: " << stream.error().message() << std::endl;
+			return 1;
+		}
+
+		// send message to server
+		stream << "Hello from client";
+		stream << std::endl; // send new-line to end receiver'stream getline
+
+		// receive message from server
+		std::string line;
+		std::getline(stream, line);
+		std::cout << line << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
 }
