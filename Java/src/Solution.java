39f0e3ed47ed2d4cf2514928bf3fdbc1e7d5ab2b
andongmin <andongmin94@gmail.com>
2023-12-19 17:21:15 +0900

    .

--- Java/src/Solution.java
+++ Java/src/Solution.java
@@ -1,7 +1,13 @@
 import java.util.*;
 
 class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        return answer;
+    }
     public static void main(String[] args) {
+        int a;
+        int b;
 
     }
 }
