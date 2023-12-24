01983724b329baad0e8a23f548e406e3080b294e
andongmin <andongmin94@gmail.com>
2023-12-25 04:11:15 +0900

    .

---
+++ C++/알고리즘/SSAFY/배열 (DAT, 방향배열) 2/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	char str[100] = "ACBIKETLAAAAABBBIIK";
+
+	int DAT[256][256] = {0,};
+	// index : 문자 -> value : 존재하는가?
+	//						 + 개수
+	//						 => 어느 위치에 존재하는가?
+	// data를 2개씩 활용한다면
+
+	for (int i = 1; str[i]; i++)
+		DAT[str[i-1]][str[i]]++; // str[i]문자의 개수를 기록
+
+	cout << DAT['A']['A'] << '\n';
+	cout << DAT['I']['K'] << '\n';
+	cout << DAT['L']['A'] << '\n';
+	cout << DAT['X']['X'] << '\n';
+}
