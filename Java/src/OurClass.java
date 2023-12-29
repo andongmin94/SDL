b4f0036dda9ab78307b28069de34238542c84e3a
김상훈 <k1016h@naver.com>
2023-12-29 14:37:24 +0900

    .

---
+++ Java/src/OurClass.java
@@ -0,0 +1,10 @@
+public class OurClass {
+
+    private final boolean just = true;
+
+    public void thisMethodIsDangerous() throws OurBadException {
+        if (just) {
+            throw new OurBadException();
+        }
+    }
+}
