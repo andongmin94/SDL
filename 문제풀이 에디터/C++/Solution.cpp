d7a3028aab11544271136bc60bbca89b5317dca7
김상훈 <k1016h@naver.com>
2024-01-06 19:14:18 +0900

    Update Solution.cpp

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -9,17 +9,12 @@ int main()
 
     int n;
     cin >> n;
-    int height[500001] = {0,};
-    stack<int> s;
 
     for (int i = 1; i <= n; i++)
     {
-        cin >> height[i];
-        while (!s.empty() && height[s.top()] < height[i])
-            s.pop();
+        cout << i << " ";
 
-        if (!s.empty()) cout << s.top() + 1 << ' ';
-        else cout << 0 << ' ';
-        s.push(i);
+        if (i % 6 == 0 || i == n)
+            cout << "Go! ";
     }
 }
