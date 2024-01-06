b4562a2055bb97ece3e2e3bd70ac6cb5c06519d4
andongmin94 <andongmin94@gmail.com>
2024-01-06 18:50:27 +0900

    Update Main.cpp

--- C++/홍정모/자료구조/챕터 01. 기본 개념/01.03 버블 정렬/Main.cpp
+++ C++/홍정모/자료구조/챕터 01. 기본 개념/01.03 버블 정렬/Main.cpp
@@ -22,9 +22,9 @@ void Print(int* arr, int size)
 
 int main()
 {
-	int arr[] = { 5, 1, 4, 2, 8 }; // 위키피디아 예시
-	// int arr[] = { 5, 4, 3, 2, 1 }; // Worst Case
-	// int arr[] = { 1, 2, 3, 5, 4 }; // Best Case
+	//int arr[] = { 5, 1, 4, 2, 8 }; // 위키피디아 예시
+	int arr[] = { 5, 4, 3, 2, 1 }; // Worst Case
+	 //int arr[] = { 1, 2, 3, 5, 4 }; // Best Case
 	int n = sizeof(arr) / sizeof(arr[0]);
 
 	Print(arr, n);
@@ -35,6 +35,37 @@ int main()
 		// 힌트: Print()로 진행상황 확인하면서 구현
 		// 힌트: 2중 for 루프
 		// 힌트: for 루프 범위 주의
+
+		// 버블 소트st
+		for (int i = 0; i < n - 1; i++)
+		{
+			bool swapped = false;
+			for (int j = 0; j < n - i - 1; j++)
+			{
+				if (arr[j] > arr[j + 1])
+				{
+					swap(arr[j], arr[j + 1]);
+					swapped = true;
+				}
+				Print(arr, n);
+			}
+			cout << endl;
+
+			if (swapped == false)
+				break;
+		}
+
+		// 셀렉션 소트st
+		//for (int i = 0; i < n - 1; i++)
+		//{
+		//	for (int j = i + 1; j < n; j++)
+		//	{
+		//		if (arr[i] > arr[j])
+		//			swap(arr[i], arr[j]);
+		//		Print(arr, n);
+		//	}
+		//	cout << endl;
+		//}
 	}
 
 	// Best case
