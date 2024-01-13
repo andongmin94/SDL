a7e6d52ae92a475bd28ec53689e0fa62574080c5
김상훈 <k1016h@naver.com>
2024-01-13 22:01:05 +0900

    Update Solution.cpp

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -7,14 +7,22 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
+    int k;
+    cin >> k;
+    for (int i = 0; i < k; i++)
     {
-        string j, k;
-        cin >> j >> k;
-        if (j == k)
-            cout << "OK" << '\n';
-        else cout << "ERROR" << '\n';
+        int n, s, d;
+        cin >> n >> s >> d;
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            int d2, v;
+            cin >> d2 >> v;
+
+            if (s * d >= d2)
+                sum += v;
+        }
+        cout << "Data Set " << i + 1 << ":" << '\n';
+        cout << sum << '\n' << '\n';
     }
 }
