0d71f09455ab4e5e1e61595710e4ede8c407332a
김상훈 <k1016h@naver.com>
2024-01-19 01:08:06 +0900

    .

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -12,26 +12,26 @@ int main()
 
     queue<pair<int, int>> Q;
     int max_v = 0;
-    bool minus_one = true;
+    bool minus_one = false;
     bool zero = true;
 
     int x, y;
     cin >> y >> x;
 
-    for (int i = 0; i < y; i++)
-    {
-        for (int j = 0; j < x; j++)
+    for (int i = 0; i < x; i++)
+        for (int j = 0; j < y; j++)
         {
             cin >> board[i][j];
             if (board[i][j] == 0)
                 zero = false;
             if (board[i][j] == 1)
             {
-                Q.push({ i, j });
+                Q.push({i, j});
                 vis[i][j] = 1;
             }
+            if (board[i][j] == -1)
+                vis[i][j] = -1;
         }
-    }
 
     int dx[4] = { 0,1,0,-1 };
     int dy[4] = { 1,0,-1,0 };
@@ -42,30 +42,25 @@ int main()
         Q.pop();
         for (int i = 0; i < 4; i++)
         {
-            int ny = cur.second + dy[i];
             int nx = cur.first + dx[i];
-            if (nx >= 0 && nx < x && ny >= 0 && ny < y && board[ny][nx] == 0 && vis[ny][nx] == 0)
+            int ny = cur.second + dy[i];
+            if (nx >= 0 && nx < x && ny >= 0 && ny < y && board[nx][ny] == 0 && vis[nx][ny] == 0)
             {
-                Q.push({ ny, nx });
-                vis[ny][nx] = vis[cur.first][cur.second] + 1;
+                Q.push({ nx, ny });
+                vis[nx][ny] = vis[cur.first][cur.second] + 1;
             }
         }
     }
 
-    for (int i = 0; i < y; i++)
-    {
-        for (int j = 0; j < x; j++)
+    for (int i = 0; i < x; i++)
+        for (int j = 0; j < y; j++)
         {
             max_v = max(max_v, vis[i][j]);
             if (vis[i][j] == 0)
-                minus_one = false;
-            cout << vis[i][j];
+                minus_one = true;
         }
-        cout << '\n';
-    }
-    cout << minus_one;
 
-    if (zero)
+    if (zero == true)
         cout << 0;
     else if (minus_one)
         cout << -1;
