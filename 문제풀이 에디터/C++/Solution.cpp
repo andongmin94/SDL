a40acb72d007ea3b10c39609f69f6fe583c86ded
김상훈 <k1016h@naver.com>
2024-01-18 20:16:09 +0900

    .

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -2,21 +2,72 @@
 
 using namespace std;
 
+int board[1000][1000];
+int vis[1000][1000] = { 0, };
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int a, b, c, d, e, f, g, h;
 
-    cin >> a >> b;
+    queue<pair<int, int>> Q;
+    int max_v = 0;
+    bool minus_one = true;
+    bool zero = true;
 
-    c = 100 - a;
-    d = 100 - b;
-    e = 100 - (c + d);
-    f = c * d;
-    g = f / 100;
-    h = f % 100;
-    cout << c << ' ' << d << ' ' << e << ' ' << f << ' ' << g << ' ' << h << '\n';
-    cout << e + g << ' ' << h << '\n';
+    int x, y;
+    cin >> y >> x;
+
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            cin >> board[i][j];
+            if (board[i][j] == 0)
+                zero = false;
+            if (board[i][j] == 1)
+            {
+                Q.push({ i, j });
+                vis[i][j] = 1;
+            }
+        }
+    }
+
+    int dx[4] = { 0,1,0,-1 };
+    int dy[4] = { 1,0,-1,0 };
+
+    while (!(Q.empty()))
+    {
+        pair<int, int> cur = Q.front();
+        Q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int ny = cur.second + dy[i];
+            int nx = cur.first + dx[i];
+            if (nx >= 0 && nx < x && ny >= 0 && ny < y && board[ny][nx] == 0 && vis[ny][nx] == 0)
+            {
+                Q.push({ ny, nx });
+                vis[ny][nx] = vis[cur.first][cur.second] + 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            max_v = max(max_v, vis[i][j]);
+            if (vis[i][j] == 0)
+                minus_one = false;
+            cout << vis[i][j];
+        }
+        cout << '\n';
+    }
+    cout << minus_one;
+
+    if (zero)
+        cout << 0;
+    else if (minus_one)
+        cout << -1;
+    else cout << max_v - 1;
 }
