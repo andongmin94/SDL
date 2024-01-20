5552a9405fee544fb3cfe86ffd5e56767267c4eb
김상훈 <k1016h@naver.com>
2024-01-21 00:24:26 +0900

    .

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -7,10 +7,8 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int a, x, b, y;
-    cin >> a >> x >> b >> y;
-    int t;
-    cin >> t;
-
-    cout << (t - 30 > 0 ? (t - 30) * x * 21  + a : a) << " " << (t - 45 > 0 ? (t - 45) * y * 21 + b : b);
+    long double a;
+    cin >> a;
+    cout << fixed << setprecision(8);
+    cout << sqrt(a) * 4;
 }
