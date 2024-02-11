92b12b61dadc72b113131c8b6e9aa6b5d5beefd1
김상훈 <k1016h@naver.com>
2024-02-12 01:42:15 +0900

    Update Solution.cpp

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -7,16 +7,11 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N, max_ = 0;
-    cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        int a,d,g, sum = 0;
-        cin >> a >> d >> g;
-        sum = a * ( d + g );
-        if (a == (d+g))
-            sum *= 2;
-        max_ = max(max_, sum);
-    }
-    cout << max_;
+    int a, p;
+    cin >> a >> p;
+    if (a * 7 > p * 13)
+        cout << "Axel";
+    else if (a * 7 == p * 13)
+        cout << "lika";
+    else cout << "Petra";    
 }
