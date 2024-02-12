635dd3aaa0c8a2cfedbd6ea7f9edb0fa6c196651
김상훈 <k1016h@naver.com>
2024-02-13 00:54:55 +0900

    Update Solution.cpp

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -7,11 +7,18 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int a, p;
-    cin >> a >> p;
-    if (a * 7 > p * 13)
-        cout << "Axel";
-    else if (a * 7 == p * 13)
-        cout << "lika";
-    else cout << "Petra";    
+    string s;
+    cin >> s;
+    int cnt = 0;
+
+    for (int i = 0; i < s.size() - 3; i++)
+    {
+        if (s[i] == 'D')
+            if (s[i+1] == 'K')
+                if (s[i+2] == 'S')
+                    if (s[i+3] == 'H')
+                        cnt++;
+    }
+
+    cout << cnt;
 }
