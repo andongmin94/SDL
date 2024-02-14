d909199a6651a8c7502fe4d576b99d49d3923e77
김상훈 <k1016h@naver.com>
2024-02-14 13:20:19 +0900

    Update Solution.cpp

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -2,17 +2,34 @@
 
 using namespace std;
 
+int a[1000000];
+
+int ans[1000000];
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int p, q;
-    cin >> p >> q;
+    int n;
+    cin >> n;
 
-    if (p <= 50 && q <= 10)
-        cout << "White";
-    else if (q > 30)
-        cout << "Red";
-    else cout << "Yellow";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    stack<int> s;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!s.empty() && s.top() <= a[i])
+            s.pop();
+        if (s.empty())
+            ans[i] = -1;
+        else
+            ans[i] = s.top();
+        s.push(a[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << ans[i] << ' ';
 }
