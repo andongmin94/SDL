26533b2b456fed6ac3cba45ade2a4618de1f9164
김상훈 <k1016h@naver.com>
2024-02-19 00:28:49 +0900

    Update Solution.cpp

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -7,25 +7,9 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin >> n;
-
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-
-    stack<int> s;
-
-    for (int i = n - 1; i >= 0; i--)
-    {
-        while (!s.empty() && s.top() <= a[i])
-            s.pop();
-        if (s.empty())
-            ans[i] = -1;
-        else
-            ans[i] = s.top();
-        s.push(a[i]);
-    }
-
-    for (int i = 0; i < n; i++)
-        cout << ans[i] << ' ';
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a <= c && c < b)
+         cout << 1;
+    else cout << 0;
 }
