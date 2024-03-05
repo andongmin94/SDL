576ed89770c938c189f8202ed1615315708ff500
김상훈 <k1016h@naver.com>
2024-03-06 00:31:53 +0900

    .

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -2,24 +2,13 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    queue<int> q;
-
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        q.push(i + 1);
-
-    while(q.size() != 1)
-    {
-        q.pop();
-        if (q.size() == 1) break;
-        q.push(q.front());
-        q.pop();
-    }
-    cout << q.front();
+    int t = 5;
+    while (t--) cout << t << '\n';
+    
+    t = 5;
+    while (--t) cout << t << '\n';
 }
