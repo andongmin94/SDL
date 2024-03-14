0fa2f069a89a0e4382e716505a01a625e585a1bf
안동민 <andongmin94@gmail.com>
2024-03-14 10:37:23 +0900

    Update Solution.cpp

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -6,14 +6,7 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int s, d, i, l, n;
-    cin >> s >> d >> i >> l >> n;
-    int cnt = 0;
-    
-    while((s+d+i+l)/4 < n)
-    {
-        s++;
-        cnt++;
-    }
-    cout << cnt;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b +3;
 }
