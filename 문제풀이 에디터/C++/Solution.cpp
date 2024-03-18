36e157d82a5bae5ceadba80543d7b8673500fa42
김상훈 <k1016h@naver.com>
2024-03-18 23:24:24 +0900

    자료구조 이분탐색 수강

--- 문제풀이 에디터/C++/Solution.cpp
+++ 문제풀이 에디터/C++/Solution.cpp
@@ -7,63 +7,54 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-<<<<<<< Updated upstream
-    int a, b;
-    cin >> a >> b;
-    cout << a + b +3;
-=======
     stack<char> stack;
     string string;
-    
     cin >> string;
-    int sum = 0;
-    int temp = 1;
+    int sum = 0, temp = 1;
 
-    for(int i = 0; i < string.size(); i++)
+    for (int i = 0; i < string.size(); i++)
     {
-        if(string[i] == '(')
+        if (string[i] == '(')
         {
             temp *= 2;
-            stack.push(string[i]);
+            stack.push('(');
         }
 
-        if(string[i] == '[')
+        if (string[i] == '[')
         {
             temp *= 3;
-            stack.push(string[i]);
+            stack.push('[');
         }
-        
-        if(string[i] == ')')
+
+        if (string[i] == ')')
         {
-            if(stack.empty() || stack.top() != '(')
+            if (stack.empty() || stack.top() != '(')
             {
                 cout << 0;
                 return 0;
             }
-            
-            if(i != 0 && string[i - 1] == '(')
-                sum += temp;
-            
+
+            if(string[i - 1] == '(') sum += temp;
+
             stack.pop();
-            temp /= 2;     
+            temp /= 2;
         }
-        
-        if(string[i] == ']')
+
+        if (string[i] == ']')
         {
-            if(stack.empty() || stack.top() != '[')
+            if (stack.empty() || stack.top() != '[')
             {
                 cout << 0;
                 return 0;
             }
-            
-            if(i != 0 && string[i - 1] == '[')
-                sum += temp;
-            
+
+            if(string[i - 1] == '[') sum += temp;
+
             stack.pop();
             temp /= 3;
         }
     }
-    if(stack.empty()) cout << sum;
+
+    if (stack.empty()) cout << sum;
     else cout << 0;
->>>>>>> Stashed changes
 }
